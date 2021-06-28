f15a293ae314ea94762e310877201a265de252d3
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-06-28 15:44:01 -0400

    The revision to raycast is slower, hm - work needs to be done here

--- src/audio_visual/raycast.c
+++ src/audio_visual/raycast.c
@@ -17,9 +17,85 @@ inlinable int calculate_wall_tex_offset(const CastData cast_data, const VectorF 
 	}
 }
 
+void old_handle_ray(const Player player, const CastData cast_data, const int screen_x,
+	byte* const first_wall_hit, double* const smallest_wall_y, const double player_angle,
+	const double theta, const double wall_y_shift, const double full_jump_height, const VectorF dir) {
+
+	const double cos_beta = cos(player_angle - theta);
+	const double corrected_dist = cast_data.dist * cos_beta;
+	const double wall_h = settings.proj_dist / corrected_dist;
+
+	const byte point_height = current_level.get_point_height(cast_data.point, cast_data.hit);
+
+	const SDL_FRect wall = {
+		screen_x,
+		wall_y_shift - wall_h / 2.0 + full_jump_height / corrected_dist,
+		settings.ray_column_width,
+		wall_h
+	};
+
+	if (*first_wall_hit) {
+		update_z_buffer(screen_x, corrected_dist);
+		*first_wall_hit = 0;
+	}
+
+	const int max_sprite_h = current_level.walls[cast_data.point - 1].surface -> h;
+
+	int offset;
+	Sprite wall_sprite;
+
+	for (byte i = 0, first_draw_event = 1; i < point_height; i++) {
+		SDL_FRect raised_wall = wall;
+		raised_wall.y -= wall.h * i;
+
+		// completely obscured: starts under the tallest so far
+		if ((double) raised_wall.y >= *smallest_wall_y) continue;
+
+		const double raised_wall_bottom = (double) (raised_wall.y + raised_wall.h);
+		double sprite_height;
+
+		// fully visible: bottom smaller than smallest top
+		if (raised_wall_bottom <= *smallest_wall_y) {
+			sprite_height = max_sprite_h;
+
+			#ifndef PLANAR_MODE
+
+			if (i == 0) std_draw_floor(player, dir, raised_wall, cos_beta);
+
+			#endif
+		}
+
+		else { // partially obscured: bottom of wall somewhere in middle of tallest
+			const double
+				y_obscured = raised_wall_bottom - *smallest_wall_y,
+				init_raised_h = (double) raised_wall.h;
+
+			raised_wall.h -= (float) y_obscured;
+			if (doubles_eq((double) raised_wall.h, 0.0, std_double_epsilon)) continue;
+
+			sprite_height = max_sprite_h * (double) raised_wall.h / init_raised_h;
+		}
+
+		if ((double) raised_wall.y < *smallest_wall_y) *smallest_wall_y = (double) raised_wall.y;
+
+		if (first_draw_event) {
+			wall_sprite = current_level.walls[cast_data.point - 1];	
+			offset = calculate_wall_tex_offset(cast_data, dir, wall_sprite.surface -> w);
+			const byte shade = 255 * calculate_shade((double) wall.h, cast_data.hit);
+			SDL_SetTextureColorMod(wall_sprite.texture, shade, shade, shade);
+			first_draw_event = 0;
+		}
+
+		const SDL_Rect slice = {offset, 0, 1, sprite_height};
+		SDL_RenderCopyF(screen.renderer, wall_sprite.texture, &slice, &raised_wall);
+	}
+}
+
 void handle_ray(const Player player, const CastData cast_data, const int screen_x,
 	byte* const first_wall_hit, double* const smallest_wall_y, const double player_angle,
 	const double theta, const double wall_y_shift, const double full_jump_height, const VectorF dir) {
+
+	// this one is slower
 
 	#ifdef PLANAR_MODE
 	(void) player;
@@ -43,8 +119,6 @@ void handle_ray(const Player player, const CastData cast_data, const int screen_
 		*first_wall_hit = 0;
 	}
 
-	// const int max_sprite_h = current_level.walls[cast_data.point - 1].surface -> h;
-
 	int offset;
 	Sprite wall_sprite;
 
@@ -57,30 +131,25 @@ void handle_ray(const Player player, const CastData cast_data, const int screen_
 
 		const double raised_wall_bottom = (double) (raised_wall.y + raised_wall.h);
 
-		if (raised_wall_bottom > *smallest_wall_y) { // partially obscured: bottom of wall somewhere in middle of tallest
+		// partially obscured: bottom of wall somewhere in middle of tallest
+		if (raised_wall_bottom > *smallest_wall_y) {
 			const double y_obscured = raised_wall_bottom - *smallest_wall_y;
 			raised_wall.h -= (float) y_obscured;
 			if (doubles_eq((double) raised_wall.h, 0.0, std_double_epsilon)) continue;
 		}
-
-		#ifndef PLANAR_MODE
-
-		else if (i == 0) std_draw_floor(player, dir, raised_wall, cos_beta);
-
-		#endif
 
 		if ((double) raised_wall.y < *smallest_wall_y) *smallest_wall_y = (double) raised_wall.y;
 
 		if (first_draw_event) {
 			wall_sprite = current_level.walls[cast_data.point - 1];	
 			offset = calculate_wall_tex_offset(cast_data, dir, wall_sprite.surface -> w);
+
 			const byte shade = 255 * calculate_shade((double) wall.h, cast_data.hit);
 			SDL_SetTextureColorMod(wall_sprite.texture, shade, shade, shade);
-			first_draw_event = 0;
 
-			#ifndef PLANAR_MODE
-			if (i == 0) std_draw_floor(player, dir, raised_wall, cos_beta);
-			#endif
+			std_draw_floor(player, dir, raised_wall, cos_beta);
+
+			first_draw_event = 0;
 		}
 
 		const SDL_Rect slice = {offset, 0, 1, wall_sprite.surface -> h};
@@ -157,7 +226,7 @@ void raycast(const Player player, const double wall_y_shift, const double full_j
 			const byte point = map_point(current_level.wall_data, curr_tile.x, curr_tile.y);
 			if (point) {
 				const CastData cast_data = {point, side, distance, VectorF_line_pos(player.pos, dir, distance)};
-				handle_ray(player, cast_data, screen_x, &first_wall_hit, &smallest_wall_y,
+				old_handle_ray(player, cast_data, screen_x, &first_wall_hit, &smallest_wall_y,
 					player_angle, theta, wall_y_shift, full_jump_height, dir);
 			}
 		}
