d12ad0f4c430abf0f74b7f10cc3d6e70beeede0f
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-07-16 10:47:26 +0200

    Back to level 1 again, and update gitignore

--- src/main/dungeon_dave.c
+++ src/main/dungeon_dave.c
@@ -49,7 +49,7 @@ non-clipping pathfinding
 int main(void) {
 	Player player;
 	Weapon weapon;
-	load_all_defaults(load_red_room, &player, &weapon);
+	load_all_defaults(load_level_1, &player, &weapon);
 
 	// mipmap_test();
 
