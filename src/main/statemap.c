71e4a62cad4f0937a10ed7600d9b2d8545f812bc
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-07-21 18:07:46 +0200

    More work on statemaps

--- src/main/statemap.c
+++ src/main/statemap.c
@@ -1,44 +1,48 @@
 /* a statemap is just a matrix of bits. I didn't call it
-a bitmat because that sounds too much like bitmap.
-
-also, a chunk is a 32-bit number. each chunk can hold 32 states. */
-
-typedef uint32_t statemap_chunk_t; // using _t for scalar types, and pascal case for others
+a bitmat because that sounds too much like bitmap. */
 
 typedef struct {
-	ivec chunk_size;
-	statemap_chunk_t* const data;
+	const ivec byte_dimensions;
+	byte* data;
 } StateMap;
 
-StateMap init_statemap(const int width, const int height) {
-	const ivec dimensions_in_bytes = {
-		width / sizeof(statemap_chunk_t),
-		height / sizeof(statemap_chunk_t)
+StateMap init_statemap(const int bits_across, const int bits_down) {
+	StateMap statemap = {
+		.byte_dimensions = {ceil(bits_across / 8.0), ceil(bits_down / 8.0)}
 	};
 
-	DEBUG_IVEC(dimensions_in_bytes);
+	const int total_bytes = statemap.byte_dimensions.x * statemap.byte_dimensions.y;
+	statemap.data = malloc(total_bytes);
+	memset(statemap.data, 0, total_bytes);
 
-	const size_t data_size = dimensions_in_bytes.x * dimensions_in_bytes.y / sizeof(statemap_chunk_t);
-	return (StateMap) {dimensions_in_bytes, malloc(data_size)};
+	return statemap;
 }
 
 //////////
 
-void print_bits(const statemap_chunk_t chunk) {
-	for (int i = 31; i >= 0; i--)
-		printf("%c", (chunk & (1 << i)) ? '1' : '0');
-	putchar('\n');
+byte* statemap_byte(const StateMap statemap, const int x, const int y) {
+	return statemap.data + (y * statemap.byte_dimensions.x + x);
 }
 
 void print_statemap(const StateMap statemap) {
-	(void) statemap;
+	const ivec byte_dimensions = statemap.byte_dimensions;
 
-	// 00000000000000000000000000000000 = 32 zeros
-	print_bits(0b11000000000000000000000000000001);
+	*statemap_byte(statemap, 0, 0) = 0b11111111;
+	// next: set bits on a bit scale
+
+	for (int y = 0; y < byte_dimensions.y; y++) {
+		putchar('|');
+		for (int x = 0; x < byte_dimensions.x; x++) {
+			const byte bits = *statemap_byte(statemap, x, y);
+			for (int i = 7; i >= 0; i--) printf("%c", (bits & (1 << i)) ? '1' : '0');
+			putchar('|');
+		}
+		putchar('\n');
+	}
 }
 
 void statemap_test(void) {
-	const StateMap statemap = init_statemap(32, 32); // should equal sizeof(uint64_t) * 4
+	const StateMap statemap = init_statemap(16, 16); 
 	print_statemap(statemap);
 
 	exit(0);
