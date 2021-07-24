3062483f62a0e6496e68a361fab00975e7e17bfa
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-07-24 16:59:08 +0200

    Tweak a line a bit

--- src/combat/weapon.c
+++ src/combat/weapon.c
@@ -49,8 +49,8 @@ void use_weapon_if_needed(Weapon* const weapon, const Player* const player, cons
 	}
 
 	// -1 -> cycle frame, 0 -> first frame
-	animate_weapon(&weapon -> animation, player -> pos, weapon -> paces_sideways, weapon -> in_use,
-		player -> y_pitch, player -> pace.screen_offset);
+	animate_weapon(&weapon -> animation, player -> pos, weapon -> paces_sideways,
+		weapon -> in_use, player -> y_pitch, player -> pace.screen_offset);
 }
 
 #else
