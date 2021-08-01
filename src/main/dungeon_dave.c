c34cd38367ca742a69d128d04e254ec5fa5c20cf
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-08-02 00:29:33 +0200

    Back to palace, and comment out other levels out of #includes for now

--- src/main/dungeon_dave.c
+++ src/main/dungeon_dave.c
@@ -36,17 +36,19 @@
 #include "../combat/enemy.c"
 #include "../combat/weapon.c"
 
+/*
 #include "../data/levels/level_1.c"
 #include "../data/levels/debug_level.c"
 #include "../data/levels/red_room.c"
 #include "../data/levels/forever_maze.c"
+*/
 #include "../data/levels/palace.c"
 
 // drawing order: skybox, walls, things, weapon, floor, minimap, hp, crosshair
 int main(void) {
 	Player player;
 	Weapon weapon;
-	load_all_defaults(load_maze, &player, &weapon);
+	load_all_defaults(load_palace, &player, &weapon);
 
 	if (display_title_screen() == Exit) deinit_all(&player, weapon);
 	play_sound(current_level.background_sound, 1);
