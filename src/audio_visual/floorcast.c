7efa6461ab1e82ec905274c322d14bd04d4d3912
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-08-09 16:53:11 +0200

    Fix a seam error with draw_colored_floor

--- src/audio_visual/floorcast.c
+++ src/audio_visual/floorcast.c
@@ -122,7 +122,7 @@ void fill_val_buffers_for_planar_mode(const double angle_degrees) {
 #endif
 
 void draw_colored_floor(const double y_shift) {
-	const SDL_FRect floor = {0.0, y_shift - 1.0, settings.screen_width, settings.screen_height - y_shift};
+	const SDL_FRect floor = {0.0, y_shift - 1.0, settings.screen_width, settings.screen_height - y_shift + 1.0};
 	SDL_SetRenderDrawColor(screen.renderer, 148, 107, 69, SDL_ALPHA_OPAQUE);
 	SDL_RenderFillRectF(screen.renderer, &floor);
 }
