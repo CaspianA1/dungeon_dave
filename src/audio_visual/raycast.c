4976b7e9fc15a70a60148a0a8711b744df6eb664
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-08-14 16:27:38 -0400

    Pass raycast data by reference

--- src/audio_visual/raycast.c
+++ src/audio_visual/raycast.c
@@ -19,29 +19,29 @@ inlinable int get_wall_tex_offset(const byte side, const vec hit, const vec dir,
 	return cond ? (width - 1) - offset : offset;
 }
 
-vec handle_ray(const DataRaycast d) {
-	const double cos_beta = cos(d.player_angle - d.theta);
-	const double corrected_dist = d.dist * cos_beta;
+vec handle_ray(const DataRaycast* const d) {
+	const double cos_beta = cos(d -> player_angle - d -> theta);
+	const double corrected_dist = d -> dist * cos_beta;
 	const double wall_h = settings.proj_dist / corrected_dist;
 
 	const DRect wall_dest = {
-		d.screen_x,
-		d.wall_y_shift - wall_h / 2.0 + d.full_jump_height / corrected_dist,
+		d -> screen_x,
+		d -> wall_y_shift - wall_h / 2.0 + d -> full_jump_height / corrected_dist,
 		settings.ray_column_width,
 		wall_h
 	};
 
-	const double smallest_wall_y = wall_dest.y - (wall_h * (d.point_height - 1)); // = wall_top
+	const double smallest_wall_y = wall_dest.y - (wall_h * (d -> point_height - 1)); // = wall_top
 
-	if (d.first_wall_hit) update_val_buffers(d.screen_x, smallest_wall_y,
-		wall_dest.y + wall_dest.h, corrected_dist, cos_beta, d.dir);
+	if (d -> first_wall_hit) update_val_buffers(d -> screen_x, smallest_wall_y,
+		wall_dest.y + wall_dest.h, corrected_dist, cos_beta, d -> dir);
 
-	const Sprite wall_sprite = current_level.walls[d.point - 1];
+	const Sprite wall_sprite = current_level.walls[d -> point - 1];
 	const SDL_Rect mipmap_crop = get_mipmap_crop_from_wall(&wall_sprite, wall_h);
 	const int max_sprite_h = mipmap_crop.h;
 
 	SDL_Rect slice = {
-		.x = get_wall_tex_offset(d.side, d.hit, d.dir, mipmap_crop.w) + mipmap_crop.x,
+		.x = get_wall_tex_offset(d -> side, d -> hit, d -> dir, mipmap_crop.w) + mipmap_crop.x,
 		.y = mipmap_crop.y, .w = 1
 	};
 
@@ -50,26 +50,26 @@ vec handle_ray(const DataRaycast d) {
 	SDL_SetTextureColorMod(wall_sprite.texture, shade, shade, shade);
 	#endif
 
-	for (byte i = *d.last_point_height; i < d.point_height; i++) {
+	for (byte i = *d -> last_point_height; i < d -> point_height; i++) {
 		DRect raised_wall_dest = wall_dest;
 		raised_wall_dest.y -= wall_dest.h * i;
 
 		// completely obscured: starts under the tallest wall so far; wouldn't be seen, but for more speed
-		if (raised_wall_dest.y >= *d.last_wall_y || raised_wall_dest.y >= settings.screen_height)
+		if (raised_wall_dest.y >= *d -> last_wall_y || raised_wall_dest.y >= settings.screen_height)
 			continue;
 
 		// partially obscured: bottom of wall somewhere in middle of tallest
-		else if (raised_wall_dest.y + raised_wall_dest.h > *d.last_wall_y) {
-			raised_wall_dest.h = *d.last_wall_y - raised_wall_dest.y;
+		else if (raised_wall_dest.y + raised_wall_dest.h > *d -> last_wall_y) {
+			raised_wall_dest.h = *d -> last_wall_y - raised_wall_dest.y;
 			slice.h = ceil(max_sprite_h * raised_wall_dest.h / wall_h);
 		}
 		else slice.h = max_sprite_h;
 
-		*d.last_wall_y = raised_wall_dest.y;
+		*d -> last_wall_y = raised_wall_dest.y;
 		SDL_FRect frect = {raised_wall_dest.x, raised_wall_dest.y, raised_wall_dest.w, raised_wall_dest.h};
 		SDL_RenderCopyF(screen.renderer, wall_sprite.texture, &slice, &frect);
 	}
-	*d.last_point_height = d.point_height;
+	*d -> last_point_height = d -> point_height;
 	return (vec) {smallest_wall_y, wall_h};
 }
 
@@ -92,9 +92,18 @@ void raycast(const Player* const player, const double wall_y_shift, const double
 			if (point_height != curr_point_height) {
 				double height_change_y, height_change_h;
 				if (point) {
+					const DataRaycast raycast_data = {
+						&last_wall_y, player_angle, theta, ray.dist, wall_y_shift, full_jump_height,
+						player -> pos, hit, dir, point, point_height, ray.side, at_first_hit, &last_point_height, screen_x
+					};
+
+					/*
 					const vec wall_y_components = handle_ray((DataRaycast) {
 						&last_wall_y, player_angle, theta, ray.dist, wall_y_shift, full_jump_height,
 						player -> pos, hit, dir, point, point_height, ray.side, at_first_hit, &last_point_height, screen_x});
+					*/
+
+					const vec wall_y_components = handle_ray(&raycast_data);
 
 					height_change_y = wall_y_components[0], height_change_h = wall_y_components[1];
 
