e5889849a2c8d3e62f153f76ea49b310aaaa5dde
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-09-07 09:14:37 -0400

    on_click_fn_t's can now return input statuses. The menu is rendered better now. Also, the input statuses can make the menu exit. Hovering the mouse over an input status will highlight it.

--- src/audio_visual/gui/menu.c
+++ src/audio_visual/gui/menu.c
@@ -1,5 +1,5 @@
 typedef SDL_Rect (*pos_and_size_fn_t) (void);
-typedef void (*on_click_fn_t) (void);
+typedef InputStatus (*on_click_fn_t) (void);
 
 typedef struct {
 	pos_and_size_fn_t pos_and_size_fn;
@@ -52,20 +52,33 @@ void deinit_menu(const Menu* const menu) {
 	TTF_CloseFont(menu -> font);
 }
 
-void render_menu(const Menu* const menu) {
+InputStatus render_menu(const Menu* const menu) {
 	const Color3 bg_color = menu -> bg_color, fg_color = menu -> fg_color;
 	SDL_SetRenderDrawColor(screen.renderer, bg_color.r, bg_color.g, bg_color.b, SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(screen.renderer);
-	SDL_SetRenderDrawColor(screen.renderer, fg_color.r, fg_color.g, fg_color.b, SDL_ALPHA_OPAQUE);
 
 	for (byte i = 0; i < menu -> textbox_count; i++) {
 		Textbox* const textbox = &menu -> textboxes[i];
-		const SDL_Rect textbox_pos_and_size = textbox -> pos_and_size_fn();
-		SDL_RenderDrawRect(screen.renderer, &textbox_pos_and_size);
-		SDL_RenderCopy(screen.renderer, textbox -> rendered_text, NULL, &textbox_pos_and_size);
+		const SDL_Rect box = textbox -> pos_and_size_fn();
+
+		ivec mouse;
+		SDL_GetMouseState(&mouse.x, &mouse.y);
+
+		if (mouse.x >= box.x && mouse.x <= box.x + box.w && mouse.y >= box.y && mouse.y <= box.y + box.h) {
+			SDL_SetRenderDrawColor(screen.renderer, 255 - fg_color.r, 255 - fg_color.g, 255 - fg_color.b, SDL_ALPHA_OPAQUE);
+			if (event.type == SDL_MOUSEBUTTONDOWN) {
+				if (textbox -> on_click_fn() == Exit) return Exit;
+			}
+		}
+		else
+			SDL_SetRenderDrawColor(screen.renderer, bg_color.r, bg_color.g, bg_color.b, SDL_ALPHA_OPAQUE);
+
+		SDL_RenderFillRect(screen.renderer, &box);
+		SDL_RenderCopy(screen.renderer, textbox -> rendered_text, NULL, &box);
 	}
 
 	SDL_RenderPresent(screen.renderer);
+	return ProceedAsNormal;
 }
 
 //////////
@@ -78,13 +91,22 @@ SDL_Rect textbox_2_pos(void) {
 	return (SDL_Rect) {settings.half_screen_width, settings.half_screen_height, 150, 50};
 }
 
-void textbox_1_on_click(void) {}
-void textbox_2_on_click(void) {}
+InputStatus textbox_1_on_click(void) {
+	puts("Clicked textbox 1");
+	return Exit;
+}
+
+InputStatus textbox_2_on_click(void) {
+	puts("Clicked textbox 2");
+	return ProceedAsNormal;
+}
 
 void menu_test(void) {
 	const Menu menu = init_menu((Color3) {205, 92, 92}, (Color3) {135, 206, 235}, 2,
 		textbox_1_pos, textbox_1_on_click, "Textbox 1",
 		textbox_2_pos, textbox_2_on_click, "Textbox 2");
+
+	SDL_ShowCursor(SDL_TRUE);
 
 	byte testing_menu = 1;
 	while (testing_menu) {
@@ -92,8 +114,9 @@ void menu_test(void) {
 			if (event.type == SDL_QUIT) testing_menu = 0;
 		}
 
-		render_menu(&menu);
+		if (render_menu(&menu) == Exit) testing_menu = 0;
 	}
 
+	SDL_ShowCursor(SDL_FALSE);
 	deinit_menu(&menu);
 }
