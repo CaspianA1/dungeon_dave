b980666a5a13857edcceb9d9c95fd009c4a6a2e7
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-09-07 14:45:26 -0400

    I DID MARK_FLOOR!!!!!! It fills all floor areas (not completely based on height though, I will have to figure that out.) Remove some vars that weren't needed. The power of procedural tand methodic thinking, wow. Life is cool.

--- src/audio_visual/raycast.c
+++ src/audio_visual/raycast.c
@@ -19,7 +19,9 @@ inlinable int get_wall_tex_offset(const byte side, const vec hit, const vec dir,
 	return cond ? (width - 1) - offset : offset;
 }
 
-void handle_ray(const DataRaycast* const d) {
+void handle_ray(const DataRaycast* const d, byte* const mark_floor_space,
+	double* const last_projected_wall_top, double* const projected_wall_bottom) {
+
 	const double cos_beta = cos(d -> p_angle - d -> theta);
 	const double corrected_dist = d -> dist * cos_beta;
 	const double wall_h = settings.proj_dist / corrected_dist;
@@ -48,14 +50,14 @@ void handle_ray(const DataRaycast* const d) {
 	#endif
 
 	double wall_dest_h_sum = 0.0;
+	*last_projected_wall_top = *d -> last_wall_top;
 
 	for (byte i = *d -> last_point_height; i < d -> point_height; i++) {
 		DRect raised_wall_dest = wall_dest;
 		raised_wall_dest.y -= wall_dest.h * i;
 
 		// completely obscured: starts under the tallest wall so far; shouldn't be seen
-		if (raised_wall_dest.y >= *d -> last_wall_top || raised_wall_dest.y >= settings.screen_height)
-			continue;
+		if (raised_wall_dest.y >= *d -> last_wall_top) continue;
 
 		// partially obscured: bottom of wall somewhere in middle of tallest
 		else if (raised_wall_dest.y + raised_wall_dest.h > *d -> last_wall_top) {
@@ -71,25 +73,31 @@ void handle_ray(const DataRaycast* const d) {
 		SDL_RenderCopyF(screen.renderer, wall_sprite.texture, &slice, &frect);
 	}
 
-	*d -> last_point_height = d -> point_height;
+	double proj_wall_top = *d -> last_wall_top;
+	double proj_wall_bottom = proj_wall_top + wall_dest_h_sum;
 
-	double projected_wall_top = *d -> last_wall_top;
-	double projected_wall_bottom = projected_wall_top + wall_dest_h_sum;
+	if (proj_wall_bottom < 0.0 || proj_wall_top >= settings.screen_height) {
+		*mark_floor_space = 0;
+		return;
+	}
 
-	if (projected_wall_bottom < 0.0 || projected_wall_top >= settings.screen_height) return;
-	if (projected_wall_bottom >= settings.screen_height) projected_wall_bottom = settings.screen_height - 1;
-	if (projected_wall_top < 0.0) projected_wall_top = 0.0;
+	*mark_floor_space = 1;
 
-	for (int y = round(projected_wall_top); y < round(projected_wall_bottom); y++)
+	if (proj_wall_bottom >= settings.screen_height) proj_wall_bottom = settings.screen_height - 1;
+	if (proj_wall_top < 0.0) proj_wall_top = 0.0;
+
+	for (int y = round(proj_wall_top); y < round(proj_wall_bottom); y++)
 		set_statemap_bit(occluded_by_walls, wall_dest.x, y);
 
-	//////////
-	/*
-	SDL_Rect top = {wall_dest.x, projected_wall_top, 5, 5};
-	SDL_SetRenderDrawColor(screen.renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
-	SDL_RenderFillRect(screen.renderer, &top);
-	*/
-	//////////
+	*projected_wall_bottom = proj_wall_bottom;
+}
+
+void mark_floor(const DataRaycast* const d, const double last_projected_wall_top, const double projected_wall_bottom) {
+	const int x = d -> screen_x;
+
+	// floor from last projected_wall_top (red) to current projected_wall_bottom (blue) (green rgb = 0, 255, 0)
+	SDL_SetRenderDrawColor(screen.renderer, 0, 255 / (*d -> last_point_height + 1), 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderDrawLine(screen.renderer, x, last_projected_wall_top, x, projected_wall_bottom);
 }
 
 void raycast(const Player* const player, const double wall_y_shift, const double full_jump_height) {
@@ -99,8 +107,9 @@ void raycast(const Player* const player, const double wall_y_shift, const double
 		const double theta = atan((screen_x - settings.half_screen_width) / settings.proj_dist) + p_angle;
 		const vec dir = {cos(theta), sin(theta)};
 
-		double last_wall_top = DBL_MAX;
-		byte at_first_hit = 1, curr_point_height = player -> jump.height, last_point_height = player -> jump.height;
+		double last_wall_top = settings.screen_height;
+
+		byte at_first_hit = 1, last_point_height = player -> jump.height;
 		DataDDA ray = init_dda((double[2]) UNPACK_2(player -> pos), (double[2]) UNPACK_2(dir), 1.0);
 
 		while (iter_dda(&ray)) {
@@ -108,20 +117,21 @@ void raycast(const Player* const player, const double wall_y_shift, const double
 			const vec hit = vec_line_pos(player -> pos, dir, ray.dist);
 			const byte point_height = current_level.get_point_height(point, hit);
 
-			if (point_height != curr_point_height) {
+			if (last_point_height != point_height) {
 				if (point) {
 					const DataRaycast raycast_data = {
 						&last_wall_top, p_angle, theta, ray.dist, wall_y_shift, full_jump_height, player -> pos,
 						hit, dir, point, point_height, ray.side, at_first_hit, &last_point_height, screen_x
 					};
 
-					handle_ray(&raycast_data);
+					byte mark_floor_space;
+					double last_projected_wall_top, projected_wall_bottom;
+					handle_ray(&raycast_data, &mark_floor_space, &last_projected_wall_top, &projected_wall_bottom);
+					if (mark_floor_space) mark_floor(&raycast_data, last_projected_wall_top, projected_wall_bottom);
 					at_first_hit = 0;
 				}
-				else
-					last_point_height = 0;
 
-				curr_point_height = point_height;
+				last_point_height = point_height;
 			}
 		}
 	}
