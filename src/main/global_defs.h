483d3afe6f45068551efbc2233428f2518c72ba2
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-09-07 20:38:49 -0400

    Tweak some settings

--- src/main/global_defs.h
+++ src/main/global_defs.h
@@ -12,12 +12,14 @@
 
 #endif
 
-// #define PLANAR_MODE
+/*
+#define PLANAR_MODE
 #define NOCLIP_MODE
+*/
 
 #define ANTIALIAS_FIRST_MIP_LEVEL
 #define DISABLE_ENEMIES
-// #define SHADING_ENABLED
+#define SHADING_ENABLED
 // #define SOUND_ENABLED
 // #define TRACK_MEMORY
 
