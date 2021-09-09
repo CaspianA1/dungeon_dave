2dfeb737632d7bfcf6cf69269c2533d1d2f6896b
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-09-09 08:37:51 -0400

    Move GPU directory

--- unused_2/gpu/gl_sdl.c
+++ unused_2/gpu/gl_sdl.c
@@ -12,14 +12,16 @@ enum {w = 800, h = 600};
 int main(void) {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) FAIL("initialize SDL");
 
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+	int a, b, c, d, e, f, g;
+
+	a = SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	b = SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	c = SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+	d = SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
 
 	// Turn on double buffering with a 24bit Z buffer. You may need to change this to 16 or 32 for your system
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	e = SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	f = SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
 	SDL_Window* const window = SDL_CreateWindow("gl_sdl", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w, h, SDL_WINDOW_OPENGL);
 	if (window == NULL) FAIL("create a window");
@@ -28,7 +30,15 @@ int main(void) {
 	if (context == NULL) FAIL("create a context");
 
 	// This makes our buffer swap syncronized with the monitor's vertical refresh
-	SDL_GL_SetSwapInterval(1);
+	g = SDL_GL_SetSwapInterval(1);
+
+	DEBUG(a, d);
+	DEBUG(b, d);
+	DEBUG(c, d);
+	DEBUG(d, d);
+	DEBUG(e, d);
+	DEBUG(f, d);
+	DEBUG(g, d);
 
 	printf("vendor = %s\nrenderer = %s\nversion = %s\n", glGetString(GL_VENDOR), glGetString(GL_RENDERER), glGetString(GL_VERSION));
 
