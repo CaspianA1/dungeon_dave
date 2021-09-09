e44939b23ba6f440a7ce08e0c0bdc668535974fd
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-09-09 14:35:25 -0400

    Update TODO

--- src/main/dungeon_dave.c
+++ src/main/dungeon_dave.c
@@ -60,12 +60,10 @@ TODO:
 - better antialiasing
 - distance shading
 - a pause menu activated by esc, instead of escaping a window by pressing esc (screen size would be changed there too)
-- highlight menus when the mouse is over them, detect clicks, rescale stuff (maybe), and figure out the full-screen error
 - the polygon floor algorithm
 - 3D DDA pitch-angle translation
 - the rest of the trooper animations + long range AI
 - a unique hitbox size for each billboard
-- correct height translation, and then stop rendering early if possible
 - screen width and height to screen size via sublime text substitutions
 - heightmaps
 */
