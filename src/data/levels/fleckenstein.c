5fc389faca67b34f968ca688d5ab197bc0cd25ff
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-09-19 17:41:41 -0400

    New texture

--- src/data/levels/fleckenstein.c
+++ src/data/levels/fleckenstein.c
@@ -1,7 +1,6 @@
 // this is a french castle: https://www.visit.alsace/wp-content/uploads/lei/pictures/219006223-fleckenstein-castle-6-1600x900.jpg
 
 inlinable byte get_fleckenstein_point_height(const byte point, const vec pos) {
-	(void) pos;
 	switch (point) {
 		case 1:
 			if (pos[1] < 19.0001) return 5;
--- src/main/dungeon_dave.c
+++ src/main/dungeon_dave.c
@@ -85,7 +85,7 @@ int main(void) {
 	if (display_title_screen() == Exit) deinit_all(&player, &weapon);
 
 	play_sound(&current_level.background_sound, 1);
-	ground = init_pix_sprite("assets/walls/grass.bmp");
+	ground = init_pix_sprite("assets/walls/greece.bmp");
 
 	while (1) {
 		const Uint32 before = SDL_GetTicks();
