e0ae4baa77ea11bd3e6311054efac9bc9ef78354
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-10-26 11:29:28 -0400

    Clean up some code in demo 6.

--- src/audio_visual/accel/demo_6.c
+++ src/audio_visual/accel/demo_6.c
@@ -1,20 +1,23 @@
 #include "demo_5.c"
 
 /*
-Functions:
-- create_rect_plane_vertices
-- create_cuboid_vertices
-- send integral points to the gpu
+Functions to make:
+- void create_cuboid(const vec3 origin, const vec3 size);
+- GLfloat* create_hori_plane(const vec3 top_left_corner, const int width, const int height);
+- GLfloat* join_plane_vertices(const GLfloat* const plane_1, const GLfloat* const plane_2);
+
+Other stuff:
+- send integral points to the gpu + plane functions top left corner -> just ints
 */
 
+enum {plane_vertex_floats = 18, plane_uv_floats = 12};
+
 const size_t
-	plane_vertex_bytes = 18 * sizeof(GLfloat),
-	plane_uv_bytes = 12 * sizeof(GLfloat);
+	plane_vertex_bytes = plane_vertex_floats * sizeof(GLfloat),
+	plane_uv_bytes = plane_uv_floats * sizeof(GLfloat);
 
-void create_uv_for_plane(const int width, const int height, GLfloat** const uv_data) {
-	*uv_data = malloc(plane_uv_bytes);
-
-	const GLfloat uv[] = {
+GLfloat* create_uv_for_plane(const int width, const int height) {
+	const GLfloat uv[plane_uv_floats] = {
 		0.0f, 0.0f,
 		width, 0.0f,
 		0.0f, height,
@@ -24,19 +27,19 @@ void create_uv_for_plane(const int width, const int height, GLfloat** const uv_d
 		width, 0.0f
 	};
 
-	memcpy(*uv_data, uv, plane_uv_bytes);
+	GLfloat* const uv_data = malloc(plane_uv_bytes);
+	memcpy(uv_data, uv, plane_uv_bytes);
+	return uv_data;
 }
 
-// top_left_corner should be composed of just integers
-void create_vert_rect_plane(const vec3 top_left_corner, const int width, const int height,
-	GLfloat** const vertex_data) {
+#define PLANE_CREATOR_FUNCTION(type) GLfloat* create_##type##_plane(const vec3 top_left_corner,\
+	const int width, const int height)
 
-	*vertex_data = malloc(plane_vertex_bytes);
-
+PLANE_CREATOR_FUNCTION(vert) {
 	const float top_left_x = top_left_corner[0], top_left_y = top_left_corner[1], z = top_left_corner[2];
 	const float top_right_x = top_left_x + width, bottom_left_y = top_left_y - height;
 
-	const GLfloat vertices[] = {
+	const GLfloat vertices[plane_vertex_floats] = {
 		top_left_x, top_left_y, z,
 		top_right_x, top_left_y, z,
 		top_left_x, bottom_left_y, z,
@@ -46,19 +49,10 @@ void create_vert_rect_plane(const vec3 top_left_corner, const int width, const i
 		top_right_x, top_left_y, z
 	};
 
-	memcpy(*vertex_data, vertices, plane_vertex_bytes);
+	GLfloat* const vertex_data = malloc(plane_vertex_bytes);
+	memcpy(vertex_data, vertices, plane_vertex_bytes);
+	return vertex_data;
 }
-
-void create_hori_rect_plane(const vec3 top_left_corner, const int width, const int height,
-	GLfloat** const vertex_data) {
-
-	(void) top_left_corner;
-	(void) width;
-	(void) height;
-	(void) vertex_data;
-}
-
-void create_cuboid(vec3 origin, vec3 size);
 
 StateGL demo_6_init(void) {
 	StateGL sgl;
@@ -66,11 +60,12 @@ StateGL demo_6_init(void) {
 	sgl.vertex_array = init_vao();
 	sgl.index_buffer = init_ibo(demo_3_index_data, sizeof(demo_3_index_data));
 
-	GLfloat *flat_square_vertices, *uv_data;
-	vec3 top_left_corner = {3.0f, 3.0f, -1.0f};
-	const int width = 2, height = 3;
-	create_vert_rect_plane(top_left_corner, width, height, &flat_square_vertices);
-	create_uv_for_plane(width, height, &uv_data);
+	vec3 v_top_left_corner = {3.0f, 3.0f, -1.0f};
+	const int v_width = 2, v_height = 3;
+
+	GLfloat
+		*const flat_square_vertices = create_vert_plane(v_top_left_corner, v_width, v_height),
+		*const uv_data = create_uv_for_plane(v_width, v_height);
 
 	sgl.num_vertex_buffers = 2;
 	sgl.vertex_buffers = init_vbos(sgl.num_vertex_buffers,
