e2e8938e78bdf34a7204277ee3d32dda24dbfde9
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-10-31 16:16:32 -0400

    More progress on demo 11; two triangles drawn so far

--- src/audio_visual/accel/src/demos/demo_11.c
+++ src/audio_visual/accel/src/demos/demo_11.c
@@ -1,11 +1,12 @@
-#include "demo_4.c"
+#include "demo_5.c"
 
 // demo 10, but just with triangles in correct winding order
 
 typedef GLubyte plane_type_t;
 #define PLANE_TYPE_ENUM GL_UNSIGNED_BYTE
 
-enum {vars_per_vertex = 5, vertices_per_plane = 6, planes_per_mesh = 5};
+// vert per plane 6 before
+enum {vars_per_vertex = 5, vertices_per_plane = 6, planes_per_mesh = 1};
 enum {vars_per_plane = vars_per_vertex * vertices_per_plane};
 enum {bytes_per_plane = vars_per_plane * sizeof(plane_type_t)};
 enum {bytes_per_sector_mesh = bytes_per_plane * planes_per_mesh};
@@ -15,6 +16,24 @@ plane_type_t* create_sector_mesh(const plane_type_t origin[3], const plane_type_
 	(void) size;
 
 	plane_type_t* const sector_mesh = malloc(bytes_per_sector_mesh);
+
+	const plane_type_t size_x = size[0], size_y = size[1], size_z = size[2];
+
+	const plane_type_t near_x = origin[0], top_y = origin[1], near_z = origin[2];
+	const plane_type_t far_x = near_x + size_x, bottom_y = top_y - size_y, far_z = near_z + size_z;
+
+	const plane_type_t tri_pair_1[vars_per_plane] = { // extends along Z axis
+		near_x, top_y, near_z, 0.0f, 0.0f,
+		near_x, top_y, far_z, size_z, 0.0f,
+		near_x, bottom_y, near_z, 0.0f, size_y,
+
+		far_x, top_y, near_z, 0.0f, 0.0f,
+		far_x, top_y, far_z, size_z, 0.0f,
+		far_x, bottom_y, near_z, 0.0f, size_y
+	};
+
+	memcpy(sector_mesh, tri_pair_1, sizeof(tri_pair_1));
+
 	return sector_mesh;
 }
 
@@ -49,7 +68,10 @@ StateGL demo_11_init(void) {
 }
 
 void demo_11_drawer(const StateGL* const sgl) {
-	(void) sgl;
+	move(sgl -> shader_program);
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f); // Dark blue
+	enum {num_meshes = 1};
+	draw_triangles(num_meshes * planes_per_mesh * 2); // 2 = 2 triangles per plane
 }
 
 #ifdef DEMO_11
