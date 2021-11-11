13ba95003d5d0467482a5312ba5b43edca95de7f
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-11-11 14:15:46 -0500

    Add some more skybox textures

--- src/audio_visual/accel/src/demos/demo_15.c
+++ src/audio_visual/accel/src/demos/demo_15.c
@@ -141,7 +141,7 @@ StateGL demo_15_init(void) {
 
 	//////////
 	sgl.num_textures = 0;
-	const GLuint skybox_texture = init_skybox_texture("assets/snow.bmp");
+	const GLuint skybox_texture = init_skybox_texture("assets/night.bmp");
 	sgl.any_data = (void*) (uint64_t) skybox_texture;
 
 	const GLuint shader_texture_sampler = glGetUniformLocation(sgl.shader_program, "texture_sampler");
