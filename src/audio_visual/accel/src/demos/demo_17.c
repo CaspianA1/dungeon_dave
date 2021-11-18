798ed98054f7c2590ec008d57f66004b4ef20d9b
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-11-18 13:05:39 -0500

    Work more on demo 17

--- src/audio_visual/accel/src/demos/demo_17.c
+++ src/audio_visual/accel/src/demos/demo_17.c
@@ -4,8 +4,63 @@
 #include "../sector_mesh.c"
 #include "../sector.c"
 
+typedef enum { // NS = north-sorth, and EW = east-west
+	Flat, Vert_NS, Vert_EW
+} FaceType;
+
+typedef struct {
+	const FaceType type;
+	const byte origin[2]; // 3rd component specified by type
+	byte size[2];
+} Face;
+
+// Assumes that the faces are vertical
+
+/*
+byte vert_faces_are_joinable(const Face f1, const Face f2) {
+	if (f1.type != f2.type) return 0;
+
+	else if (f1.type == Vert_NS) {
+		puts("Vert type 1");
+	}
+	else {
+		puts("Vert type 2");
+	}
+
+	return 1;
+}
+*/
+
+void init_vert_ew_faces(const Sector sector, byte* const heightmap, const byte map_width) {
+		// This is for top side right now
+
+	if (sector.origin[0] == 0) return;
+
+	// Store last adjacent height
+
+	const byte y_above = sector.origin[1] - 1;
+
+	for (byte x = sector.origin[0]; x < sector.origin[0] + sector.size[0]; x++) {
+		const byte adjacent_height = *map_point(heightmap, x, y_above, map_width);
+		(void) adjacent_height;
+	}
+}
+
 #ifdef DEMO_17
 int main(void) {
+	DEBUG(sizeof(Face), zu);
+	DEBUG(sizeof(FaceType), zu);
+	DEBUG(sizeof(Face*), zu);
 
+	enum {test_map_width = 8, tes_map_height = 5};
+	static const byte test_heightmap[tes_map_height][test_map_width] = {
+		{0, 2, 2, 0, 0, 0, 0, 0},
+		{0, 2, 2, 1, 1, 1, 0, 0},
+		{0, 2, 2, 1, 1, 1, 0, 0},
+		{3, 3, 0, 0, 0, 0, 0, 0},
+		{3, 3, 0, 0, 0, 0, 0, 0}
+	};
+
+	(void) test_heightmap;
 }
 #endif
