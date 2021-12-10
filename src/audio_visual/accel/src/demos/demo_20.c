181a76a50eaa0398818b2f0e617e695d9f0ead9c
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-12-10 17:58:14 -0500

    Work more on demo 20

--- src/audio_visual/accel/src/demos/demo_20.c
+++ src/audio_visual/accel/src/demos/demo_20.c
@@ -12,21 +12,40 @@ typedef struct {
 	// 2 bytes of padding
 } Billboard;
 
+// Num billboards, billboards
+DrawableSet init_billboard_list(const size_t num_billboards, ...) {
+	DrawableSet billboard_list = {0};
+	// Def cpu and gpu buffer
+
+	va_list args;
+	va_start(args, num_billboards);
+
+	for (size_t i = 0; i < num_billboards; i++) {
+		Billboard billboard = va_arg(args, Billboard);
+		(void) billboard;
+	}
+
+	va_end(args);
+	return billboard_list;
+}
+
 StateGL demo_20_init(void) {
 	StateGL sgl = {.vertex_array = init_vao()};
 
-	List billboards = init_list(2, Billboard);
-	const Billboard first = {{2.5f, 3.5f, 4.5f}, 0}, second = {{0.0f, 1.0f, 2.0f}, 1};
-	push_ptr_to_list(&billboards, &first);
-	push_ptr_to_list(&billboards, &second);
+	DrawableSet billboard_list = init_billboard_list(2,
+		(Billboard) {{2.5f, 3.5f, 4.5f}, 0},
+		(Billboard) {{0.0f, 1.0f, 2.0f}, 1}
+	);
+
+	(void) billboard_list;
+	puts("Now do something with the billboard list");
+
 
 	/*
 	- In DrawableSet, put shader, texture set, a list of all billboards, and their indices
 	- TODO: different billboard shader that can handle many billboards as a stream
 	- TODO: function init_billboard_list
 	*/
-
-	deinit_list(billboards);
 
 	return sgl;
 }
