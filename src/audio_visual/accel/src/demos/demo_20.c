05bb14278bbf7aee98deaa3aaa126e69468bb3ec
Caspian Ahlberg <caspianahlberg@gmail.com>
2021-12-24 21:16:58 -0600

    Work on billboard frustum culling. Remove cpu_buffer_data.

--- src/audio_visual/accel/src/demos/demo_20.c
+++ src/audio_visual/accel/src/demos/demo_20.c
@@ -11,14 +11,6 @@
 - To begin with, just draw all in one big unbatched buffer
 - Batching after
 - Only using instancing for glVertexAttribDivisor
-
-Sphere intersection with plane:
-
-bool is_inside_plane(plane):
-	return plane.getSignedDistanceToPlane(self.center) > -self.radius;
-
-float getSignedDistanceToPlane(point):
-	return dot(self.normal, point) - self.distance;
 */
 
 const char* const batching_billboard_vertex_shader =
@@ -74,6 +66,39 @@ typedef struct {
 	bb_pos_component_t size[2], pos[3];
 } Billboard;
 
+/*
+Sphere intersection with plane:
+
+bool is_inside_plane(plane):
+	return plane.getSignedDistanceToPlane(self.center) > -self.radius;
+
+float getSignedDistanceToPlane(point):
+	return dot(self.normal, point) - self.distance;
+*/
+
+typedef struct {
+	const float radius;
+	vec3 center;
+} Sphere;
+
+byte is_inside_plane(Sphere sphere, vec4 plane) {
+	return (glm_vec3_dot(plane, sphere.center) - plane[3]) > -sphere.radius;
+}
+
+byte billboard_in_view_frustum(const Billboard billboard, vec4 frustum_planes[6]) {
+	const float half_w = billboard.size[0] * 0.5f, half_h = billboard.size[1] * 0.5f;
+
+	Sphere sphere = {
+		.radius = sqrtf(half_w * half_w + half_h * half_h),
+		.center = {billboard.pos[0], billboard.pos[1], billboard.pos[2]}
+	};
+
+	return
+		is_inside_plane(sphere, frustum_planes[0]) && is_inside_plane(sphere, frustum_planes[1]) &&
+		is_inside_plane(sphere, frustum_planes[2]) && is_inside_plane(sphere, frustum_planes[3]) &&
+		is_inside_plane(sphere, frustum_planes[4]) && is_inside_plane(sphere, frustum_planes[5]);
+}
+
 void draw_billboards(const DrawableSet* const billboard_list, const Camera* const camera) {
 	const GLuint
 		vbo = billboard_list -> dbo,
@@ -108,7 +133,7 @@ void draw_billboards(const DrawableSet* const billboard_list, const Camera* cons
 
 	glUseProgram(billboard_list -> shader);
 	glEnable(GL_BLEND);
-	glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, 10); // Each billboard 4 vertices, and 10 billboards
+	glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, 1); // Each billboard 4 vertices, and 1 billboard
 	glDisable(GL_BLEND);
 
 	for (byte i = 0; i < 3; i++) {
@@ -121,24 +146,20 @@ void draw_billboards(const DrawableSet* const billboard_list, const Camera* cons
 DrawableSet init_billboard_list(const size_t num_billboards, ...) {
 	DrawableSet billboard_list = {
 		.objects = init_list(num_billboards, Billboard),
-		.object_indices = init_list(num_billboards, buffer_index_t), // 1 index per billboard attribute set
+		// .object_indices = init_list(num_billboards, buffer_index_t), // 1 index per billboard attribute set
 		.shader = init_shader_program(batching_billboard_vertex_shader, batching_billboard_fragment_shader)
 	};
 
 	///////////
-	const size_t billboard_bytes = num_billboards * sizeof(Billboard);
-	Billboard* const cpu_billboard_data = malloc(billboard_bytes);
 	va_list args;
 	va_start(args, num_billboards);
-	for (size_t i = 0; i < num_billboards; i++) cpu_billboard_data[i] = va_arg(args, Billboard);
+	for (size_t i = 0; i < num_billboards; i++) ((Billboard*) (billboard_list.objects.data))[i] = va_arg(args, Billboard);
 	va_end(args);
 	///////////
 
 	glGenBuffers(1, &billboard_list.dbo);
 	glBindBuffer(GL_ARRAY_BUFFER, billboard_list.dbo);
-	glBufferData(GL_ARRAY_BUFFER, billboard_bytes, cpu_billboard_data, GL_STATIC_DRAW);
-
-	free(cpu_billboard_data);
+	glBufferData(GL_ARRAY_BUFFER, num_billboards * sizeof(Billboard), billboard_list.objects.data, GL_STATIC_DRAW);
 
 	return billboard_list;
 }
@@ -151,8 +172,10 @@ StateGL demo_20_init(void) {
 
 	// TODO: free this somewhere
 	DrawableSet billboard_list = init_billboard_list(
-		10,
-		(Billboard) {0, {1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+		1,
+		(Billboard) {0, {10.0f, 10.0f}, {0.0f, 0.0f, 0.0f}}
+
+		/*
 		(Billboard) {1, {1.0f, 1.0f}, {0.0f, 1.0f, 1.0f}},
 		(Billboard) {2, {1.0f, 1.0f}, {0.0f, 1.0f, 2.0f}},
 		(Billboard) {3, {1.0f, 1.0f}, {0.0f, 1.0f, 3.0f}},
@@ -162,10 +185,13 @@ StateGL demo_20_init(void) {
 		(Billboard) {7, {1.0f, 1.0f}, {0.0f, 1.0f, 7.0f}},
 		(Billboard) {8, {1.0f, 1.0f}, {0.0f, 1.0f, 8.0f}},
 		(Billboard) {9, {1.0f, 1.0f}, {0.0f, 1.0f, 9.0f}}
+		*/
 	);
 
-	billboard_list.texture_set = init_texture_set(TexNonRepeating, 3, 2, 64, 64,
+	billboard_list.texture_set = init_texture_set(TexNonRepeating, 3, 2, 512, 512,
 		"../../../../assets/objects/hot_dog.bmp",
+		// "../../../../assets/walls/hieroglyphics.bmp",
+
 		"../../../../assets/objects/teleporter.bmp",
 		"../../../../assets/objects/robot.bmp",
 
@@ -191,14 +217,23 @@ void demo_20_drawer(const StateGL* const sgl) {
 	static Skybox skybox;
 
 	if (first_call) {
-		skybox = init_skybox("../assets/oasis_upscaled.bmp"); // TODO: free skybox somewhere
-		init_camera(&camera, (vec3) {0.0f, 1.5f, -2.5f});
+		skybox = init_skybox("../assets/desert.bmp"); // TODO: free skybox somewhere
+		init_camera(&camera, (vec3) {0.0f, 0.0f, 0.0f}); // 0.0f, 1.5, -2.5f
 		first_call = 0;
 	}
 
 	update_camera(&camera, get_next_event());
 	draw_skybox(skybox, &camera);
 	draw_billboards(billboard_list, &camera);
+
+	//////////
+
+	/*
+	const Billboard hot_dog = ((Billboard*) (billboard_list -> objects.data))[0];
+	vec4 frustum_planes[6];
+	glm_frustum_planes((vec4*) camera.view_projection, frustum_planes);
+	DEBUG(billboard_in_view_frustum(hot_dog, frustum_planes), d);
+	*/
 }
 
 #ifdef DEMO_20
