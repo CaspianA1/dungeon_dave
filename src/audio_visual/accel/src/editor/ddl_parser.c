52cd4ae9edd597740899ee1e726939d1fa8debc9
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-01-04 11:58:24 -0500

    Pass FileContents by ref. Can now discard whitespace after section tag to start parsing section data.

--- src/audio_visual/accel/src/editor/ddl_parser.c
+++ src/audio_visual/accel/src/editor/ddl_parser.c
@@ -14,17 +14,24 @@ static FileContents read_file_contents(const char* const file_name) {
 	data[num_bytes] = '\0';
 
 	fclose(file);
-	return (FileContents) {data, file_name, num_bytes};
+	return (FileContents) {file_name, data, num_bytes};
 }
 
-static void parse_section_tag(const long* const char_index, const FileContents file_contents) {
+// This discards whitespace after an index until a non-whitespace character is reached
+static void discard_whitespace_after_index(long* const char_index_ref, const FileContents* const file_contents) {
+	long char_index = *char_index_ref + 1;
+	while ((char_index < file_contents -> num_bytes) && isspace(file_contents -> data[char_index])) char_index++;
+	*char_index_ref = char_index;
+}
+
+static const SectionTag* get_section_tag(const long char_index, const FileContents* const file_contents) {
 	/* While not reached space or newline, collect tag name
 	Curr tag types: name, heightmap, texture_id_map, wall_textures 
 	Also, a variant of one-line tags
 	Goal is to get correct section tag out, by comparing the string up until a newline (do the multiline variant first) */
 
 	// No need to worry about overflow here since strncmp accounts for a possible null terminator
-	const char* const tag_start = file_contents.data + *char_index + 1;
+	const char* const tag_start = file_contents -> data + char_index + 1;
 	const SectionTag* matching_tag = NULL;
 
 	for (byte i = 0; i < num_section_tag_types; i++) {
@@ -38,29 +45,49 @@ static void parse_section_tag(const long* const char_index, const FileContents f
 	}
 
 	if (matching_tag == NULL)
-		FAIL(ParseLevelFile, "did not recognize a level tag for '%s'.", file_contents.file_name);
+		FAIL(ParseLevelFile, "did not recognize a level tag for '%s'.", file_contents -> file_name);
 
 	else printf("Found a tag match: '%s'\n", matching_tag -> name);
+	return matching_tag;
 }
 
-static void parse_ddl_file(EditorState* const editor_state, const FileContents file_contents) {
-	for (long i = 0; i < file_contents.num_bytes; i++) {
+static void parse_section(const SectionTag* const tag, const EditorState* const editor_state, const FileContents* const file_contents) {
+	(void) tag;
+	(void) editor_state;
+	(void) file_contents;
 
-		const char curr_char = file_contents.data[i];
+	if (tag -> is_one_line) {
+		// Only expecting name for now; alloc map name for editor state, until any whitespace reached
+	}
+	else {
+
+	}
+}
+
+static void parse_ddl_file(EditorState* const editor_state, const FileContents* const file_contents) {
+	const char* const data = file_contents -> data;
+	const long num_bytes = file_contents -> num_bytes;
+
+	for (long i = 0; i < num_bytes; i++) {
+
+		const char curr_char = data[i];
 		switch (curr_char) {
 			case COMMENT_START: // This skips until a newline is reached
-				while (i < file_contents.num_bytes && file_contents.data[i] != '\n') i++;
+				while (i < num_bytes && data[i] != '\n') i++;
 				break;
-			case SECTION_TAG_START:
-				puts("Start of section");
-				parse_section_tag(&i, file_contents);
+			case SECTION_TAG_START: {
+				const SectionTag* const section_tag = get_section_tag(i, file_contents);
+				i += strlen(section_tag -> name); // + 1l b/c starting at the character right after the tag
+				discard_whitespace_after_index(&i, file_contents);
+				parse_section(section_tag, editor_state, file_contents);
 				break;
+			}
 		}
 	}
 }
 
 void init_editor_state_from_ddl_file(EditorState* const editor_state, const char* const filename) {
 	const FileContents file_contents = read_file_contents(filename);
-	parse_ddl_file(editor_state, file_contents);
+	parse_ddl_file(editor_state, &file_contents);
 	free(file_contents.data);
 }
--- src/audio_visual/accel/src/editor/ddl_parser.h
+++ src/audio_visual/accel/src/editor/ddl_parser.h
@@ -8,8 +8,8 @@
 
 // DDL = Dungeon Dave level. Later, keep file handle to write to it later.
 typedef struct {
-	char* const data;
 	const char* const file_name;
+	char* const data;
 	const long num_bytes;
 } FileContents;
 
@@ -26,7 +26,7 @@ static const SectionTag section_tags[num_section_tag_types] = {
 	{1, "name"}, {0, "heightmap"}, {0, "texture_id_map"}, {0, "wall_textures"}
 };
 
-////////// Excluded: read_file_contents, parse_section_tag, parse_ddl_file
+////////// Excluded: read_file_contents, discard_whitespace_after_index, get_section_tag, parse_section, parse_ddl_file
 
 void init_editor_state_from_ddl_file(EditorState* const editor_state, const char* const filename);
 
