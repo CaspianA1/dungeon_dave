427448e14f31c698f4ee039f2a658ca8099867ca
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-01-10 01:09:03 -0500

    Change build type to optimize, and change skybox to mountain 2

--- src/audio_visual/accel/src/demos/demo_17.c
+++ src/audio_visual/accel/src/demos/demo_17.c
@@ -54,7 +54,7 @@ StateGL demo_17_init(void) {
 	*/
 
 	StateGL sgl = {.vertex_array = init_vao(), .num_vertex_buffers = 0, .num_textures = 0};
-	SceneState scene_state = {.skybox = init_skybox("../assets/wadi_upscaled.bmp"),
+	SceneState scene_state = {.skybox = init_skybox("../assets/mountain_2.bmp"),
 		.heightmap = (byte*) palace_heightmap, .map_size = {palace_width, palace_height}};
 
 	//////////
