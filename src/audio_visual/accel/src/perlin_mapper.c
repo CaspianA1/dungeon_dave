446a374454f1311d86fe6003f74883731f1c8717
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-01-17 15:36:55 -0500

    Switch integer and floating point types to 64 bit, because otherwise, a higher frequency results in overflow.

--- src/audio_visual/accel/src/perlin_mapper.c
+++ src/audio_visual/accel/src/perlin_mapper.c
@@ -3,77 +3,80 @@
 #define PIXEL_FORMAT SDL_PIXELFORMAT_INDEX8
 #define DEBUG(var, format) printf(#var " = %" #format "\n", var)
 
-typedef float vec2[2];
-typedef float mat4[4][4];
+typedef uint64_t integer_t;
+typedef double floating_t;
+
+typedef floating_t vec2[2];
+typedef floating_t mat4[4][4];
 
 //////////
 
-const int first_octave = 3, octaves = 50;
+const integer_t first_octave = 1ul, octaves = 60ull;
 
-const float
-	inner_rand_multiplier = 1.43f, outer_rand_multiplier = 2.0f, rand_subtrahend = 1.0f,
-	persistence = 0.65f, scale = 1.0f, result_addend = 0.4f;
+const floating_t
+	inner_rand_multiplier = 1.51, outer_rand_multiplier = 2.0, rand_subtrahend = 1.0,
+	persistence = 0.7, scale = 1.0, result_addend = 0.4;
 
 //////////
 
-float dot(const vec2 a, const vec2 b) {
+floating_t dot(const vec2 a, const vec2 b) {
 	return a[0] * b[0] + a[1] * b[1];
 }
 
-float fract(const float a) {
-	return a - floorf(a);
+floating_t fract(const floating_t a) {
+	return a - floor(a);
 }
 
-float noise(const int x, const int y) {
+floating_t noise(const integer_t x, const integer_t y) {
 	return outer_rand_multiplier
-		* fract(sinf(inner_rand_multiplier * dot((vec2) {x, y}, (vec2) {12.9898f, 78.233f})) * 43758.5453f)
+		* fract(sin(inner_rand_multiplier * dot((vec2) {x, y}, (vec2) {12.9898, 78.233})) * 43758.5453)
 		- rand_subtrahend;
 }
 
-float lerp(const float a, const float b, const float weight) {
-	return a * (1.0f - weight) + b * weight;
+floating_t lerp(const floating_t a, const floating_t b, const floating_t weight) {
+	return a * (1.0 - weight) + b * weight;
 }
 
-float noise_from_samples(const int cx, const int cy, const mat4 samples) {
-	return samples[cy][cx] * 0.25f
-		+ (samples[cy - 1][cx] + samples[cy + 1][cx] + samples[cy][cx - 1] + samples[cy][cx + 1]) * 0.125f
-		+ (samples[cy - 1][cx - 1] + samples[cy - 1][cx + 1] + samples[cy + 1][cx - 1] + samples[cy + 1][cx + 1]) * 0.0625f;
+floating_t noise_from_samples(const integer_t cx, const integer_t cy, const mat4 samples) {
+	return samples[cy][cx] * 0.25
+		+ (samples[cy - 1ull][cx] + samples[cy + 1ull][cx] + samples[cy][cx - 1ull] + samples[cy][cx + 1ull]) * 0.125
+		+ (samples[cy - 1ull][cx - 1ull] + samples[cy - 1ull][cx + 1ull] + samples[cy + 1ull][cx - 1ull] + samples[cy + 1ull][cx + 1ull]) * 0.0625;
 }
 
-float lerp_noise(const vec2 pos) {
-	const int ix = pos[0], iy = pos[1];
+floating_t lerp_noise(const vec2 pos) {
+	const integer_t ix = pos[0], iy = pos[1];
 
 	const mat4 samples = {
-		{noise(ix - 1, iy - 1), noise(ix, iy - 1), noise(ix + 1, iy - 1), noise(ix + 2, iy - 1)},
-		{noise(ix - 1, iy),     noise(ix, iy),     noise(ix + 1, iy),     noise(ix + 2, iy)},
-		{noise(ix - 1, iy + 1), noise(ix, iy + 1), noise(ix + 1, iy + 1), noise(ix + 2, iy + 1)},
-		{noise(ix - 1, iy + 2), noise(ix, iy + 2), noise(ix + 1, iy + 2), noise(ix + 2, iy + 2)}
+		{noise(ix - 1ull, iy - 1ull), noise(ix, iy - 1ull), noise(ix + 1ull, iy - 1ull), noise(ix + 2ull, iy - 1ull)},
+		{noise(ix - 1ull, iy),        noise(ix, iy),        noise(ix + 1ull, iy),        noise(ix + 2ull, iy)},
+		{noise(ix - 1ull, iy + 1ull), noise(ix, iy + 1ull), noise(ix + 1ull, iy + 1ull), noise(ix + 2ull, iy + 1ull)},
+		{noise(ix - 1ull, iy + 2ull), noise(ix, iy + 2ull), noise(ix + 1ull, iy + 2ull), noise(ix + 2ull, iy + 2ull)}
 	};
 
-	const float fract_x = fract(pos[0]);
+	const floating_t fract_x = fract(pos[0]);
 
-	const float
-		noise_top = lerp(noise_from_samples(1, 1, samples), noise_from_samples(2, 1, samples), fract_x),
-		noise_bottom = lerp(noise_from_samples(1, 2, samples), noise_from_samples(2, 2, samples), fract_x);
+	const floating_t
+		noise_top = lerp(noise_from_samples(1ull, 1ull, samples), noise_from_samples(2ull, 1ull, samples), fract_x),
+		noise_bottom = lerp(noise_from_samples(1ull, 2ull, samples), noise_from_samples(2ull, 2ull, samples), fract_x);
 
 	return lerp(noise_top, noise_bottom, fract(pos[1]));
 }
 
-float perlin_2D(const vec2 pos) {
-	float noise = result_addend, amplitude = powf(persistence, first_octave);
-	int frequency = 2 << (first_octave - 1);
+floating_t perlin_2D(const vec2 pos) {
+	floating_t noise = result_addend, amplitude = pow(persistence, first_octave);
+	uint64_t frequency = 2ull << (first_octave - 1ull);
 
 	// Fractal brownian motion
-	for (int i = first_octave; i < octaves + first_octave; i++, frequency <<= 1, amplitude *= persistence)
+	for (integer_t i = first_octave; i < octaves + first_octave; i++, frequency <<= 1ull, amplitude *= persistence)
 		noise += lerp_noise((vec2) {pos[0] * frequency, pos[1] * frequency}) * amplitude;
 
-	if (noise > 1.0f) return 1.0f;
-	return (noise < 0.0f) ? 0.0f : noise;
+	if (noise > 1.0) return 1.0;
+	return (noise < 0.0) ? 0.0 : noise;
 }
 
 //////////
 
-SDL_Surface* make_perlin_map(const int size[2]) {
+SDL_Surface* make_perlin_map(const integer_t size[2]) {
 	SDL_Surface* const perlin_map = SDL_CreateRGBSurfaceWithFormat(0,
 		size[0], size[1], SDL_BITSPERPIXEL(PIXEL_FORMAT), PIXEL_FORMAT);
 
@@ -81,17 +84,17 @@ SDL_Surface* make_perlin_map(const int size[2]) {
 
 	enum {num_colors = 256};
 	SDL_Color palette[num_colors];
-	for (int i = 0; i < num_colors; i++) palette[i] = (SDL_Color) {i, i, i, 255};
+	for (integer_t i = 0; i < num_colors; i++) palette[i] = (SDL_Color) {i, i, i, 255};
 	SDL_SetPaletteColors(perlin_map -> format -> palette, palette, 0, num_colors);
 
-	const vec2 downscale_vals = {1.0f / size[0] * scale, 1.0f / size[1] * scale};
+	const vec2 downscale_vals = {1.0 / size[0] * scale, 1.0 / size[1] * scale};
 
 	const int bytes_per_row = perlin_map -> pitch;
 	Uint8* index_row = perlin_map -> pixels;
 
-	for (int y = 0; y < size[1]; y++, index_row += bytes_per_row) {
-		const float downscaled_y = y * downscale_vals[1];
-		for (int x = 0; x < size[0]; x++)
+	for (integer_t y = 0ull; y < size[1]; y++, index_row += bytes_per_row) {
+		const floating_t downscaled_y = y * downscale_vals[1];
+		for (integer_t x = 0ull; x < size[0]; x++)
 			index_row[x] = perlin_2D((vec2) {x * downscale_vals[0], downscaled_y}) * num_colors;
 	}
 
@@ -101,7 +104,7 @@ SDL_Surface* make_perlin_map(const int size[2]) {
 
 /*
 - This should take command line arguments later
-- 2D first, 3D later
+- 3D: http://www.science-and-fiction.org/rendering/noise.html
 - Compile this with the map code once done
 */
 
@@ -111,7 +114,7 @@ int main(void) {
 		return 1;
 	}
 
-	SDL_Surface* const perlin_map = make_perlin_map((int[2]) {256, 256});
+	SDL_Surface* const perlin_map = make_perlin_map((integer_t[2]) {1024ull, 1024ull});
 	SDL_SaveBMP(perlin_map, "out.bmp");
 	SDL_FreeSurface(perlin_map);
 
