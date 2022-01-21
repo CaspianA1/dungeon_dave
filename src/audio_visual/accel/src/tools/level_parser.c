5c39b82d01578ce4cdbc80f6ae8e566fa17451b9
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-01-21 15:48:30 -0500

    Make a version of the ddl file from before as a json file. Work on the json lexer. Make a neat PARSE_FAIL macro.

--- src/audio_visual/accel/src/tools/level_parser.c
+++ src/audio_visual/accel/src/tools/level_parser.c
@@ -14,23 +14,75 @@ static FileContents read_file_contents(const char* const file_name) {
 	data[num_bytes] = '\0';
 
 	fclose(file);
-	return (FileContents) {file_name, data};
+	return (FileContents) {file_name, data, num_bytes};
 }
 
-static void lex_json_file(void) {
+static byte is_delim(const char curr_char, const char* delims) {
+	while (*delims != '\0') {
+		if (curr_char == *delims) return 1;
+		delims++;
+	}
+	return 0;
+}
 
+static const char* tokenize(char* const to_tokenize, const char* const delims) {
+	static char* buffer;
+	if (to_tokenize != NULL) buffer = to_tokenize;
+
+	while (is_delim(*buffer, delims)) *(buffer++) = '\0';
+
+	const char* const start = buffer;
+	if (*start == '\0') return NULL; // Whitespace followed by end of file
+
+	while (!is_delim(*buffer, delims)) {
+		if (*buffer == '\0') return start;
+		buffer++;
+	}
+
+	*(buffer++) = '\0';
+	return start;
+}
+
+// Returns a list of all tokens
+static List lex_json_file(const FileContents file_contents) {
+	List tokens = init_list(1, char*);
+
+	/*
+	- Split on {}[]:,'" while still keeping them
+	- Treat everything inside a string as 1 token
+	*/
+
+	const char* const delims = " \n\t\v\f\r";
+	const char* token = tokenize(file_contents.data, delims);
+
+	while (token != NULL) {
+		push_ptr_to_list(&tokens, &token);
+		token = tokenize(NULL, delims);
+	}
+
+	/*
+	for (buffer_size_t i = 0; i < tokens.length; i++) {
+		const char* const token = ((char**) (tokens.data))[i];
+		printf("token = '%s'\n", token);
+	}
+	*/
+
+	return tokens;
 }
 
 // Sets num_textures, map_size, heightmap, texture_id_map, map_name, renderer, and textures
 static void parse_json_file(EditorState* const eds, FileContents* const file_contents) {
 	puts("About to parse json file");
+	// printf("Parse this: '%s'\n", file_contents -> data);
 	(void) eds;
-	(void) file_contents;
 
 	/*
 	1. Tokenize
 	2. Parse into tree
 	*/
+
+	const List tokens = lex_json_file(*file_contents);
+	deinit_list(tokens);
 }
 
 void init_editor_state_from_json_file(EditorState* const eds, const char* const file_name) {
--- src/audio_visual/accel/src/tools/level_parser.h
+++ src/audio_visual/accel/src/tools/level_parser.h
@@ -1,12 +1,38 @@
 #include "editor.h"
 #include "../headers/list.h"
 
+#define QUOTE(a) #a
+#define COLOR_STRING(color, format) "\033[" QUOTE(color) "m" format "\033[0m"
+
+#define RED_COLOR 32
+
+const struct {
+	const char
+		object_start, object_end,
+		array_start, array_end,
+		key_value_separator, element_delimiter,
+		string_single_quote, string_double_quote;
+} token_defs = {
+	'{', '}', '[', ']', ':', ',', '\'', '\"'
+};
+
+// Later on, the token list can perhaps be one big string, just separated by '\0' symbols
+
 typedef struct {
-	const char* const file_name;
+	const char* const name;
 	char* const data;
+	const long num_bytes;
 } FileContents;
 
-////////// Excluded: read_file_contents, lex_json_file, parse_json_file, 
+//////////
+
+#define PARSE_FAIL(file_contents, message, contents_start, contents_end) do {\
+	FAIL(ParseLevelFile, "Could not parse '%s' (%s):\n---\n" COLOR_STRING(RED_COLOR, "%.*s") "\n---",\
+		(file_contents).name, (message), (int) ((contents_end) - (contents_start)),\
+		file_contents.data + (contents_start));\
+} while (0)
+
+////////// Excluded: read_file_contents, is_delim, tokenize, lex_json_file, parse_json_file
 
 // Later on, init_level_state_from_json_file
 void init_editor_state_from_json_file(EditorState* const eds, const char* const file_name);
