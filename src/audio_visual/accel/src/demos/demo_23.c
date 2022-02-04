f6317a3e9c9d277240d329f69f26da98590ac155
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-02-04 15:08:50 -0500

    Work more on demo 23

--- src/audio_visual/accel/src/demos/demo_23.c
+++ src/audio_visual/accel/src/demos/demo_23.c
@@ -9,11 +9,32 @@ Decision to make (one to choose):
 #include "../utils.c"
 #include "../sector.c"
 
+void sector_shadow_visibility(const Sector* const sector, const vec3 light_pos) {
+	const byte *const origin = sector -> origin, *const size = sector -> size;
 
-void foo(const Sector* const sector) {
-	const byte
-		*const origin = sector -> origin,
-		*const size = sector -> size;
+	// These are top-down values
+	const byte start_x = origin[0], start_y = origin[1];
+	const byte end_x = start_x + size[0], end_y = start_y + size[1];
+
+	if (light_pos[1] > sector -> visible_heights.max) {
+		puts("Flat face visible");
+	}
+
+	if (light_pos[0] < start_x) {
+		puts("Left vert NS face visible");
+	}
+	else if (light_pos[0] > end_x) {
+		puts("Right vert NS face visible");
+	}
+
+	if (light_pos[2] < start_y) {
+		puts("Top vert EW face visible");
+	}
+	else if (light_pos[2] > end_y) {
+		puts("Bottom vert EW face visible");
+	}
+
+	// The next step is to take all invisible faces, and find the ones that are adjacent to visible faces
 }
 
 #ifdef DEMO_23
