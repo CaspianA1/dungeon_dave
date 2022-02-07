1af9f8b39550325c3159492463d94835c4f3de3a
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-02-07 12:36:10 -0500

    Now rendering scene for shadow map demo

--- src/audio_visual/accel/src/demos/demo_23.c
+++ src/audio_visual/accel/src/demos/demo_23.c
@@ -9,34 +9,101 @@
 #include "../camera.c"
 
 typedef struct {
-	GLuint
-		depth_map_framebuffer, depth_map_texture,
-		scene_vbo;
+	const GLuint scene_shader, depth_map_texture;
+	GLuint scene_vbo, depth_map_framebuffer;
 
-	buffer_size_t shadow_size[2];
+	buffer_size_t num_scene_vertices;
+	const buffer_size_t shadow_size[2];
 } SceneState;
+
+const char *const demo_22_scene_vertex_shader =
+	"#version 330 core\n"
+
+	"layout(location = 0) in vec3 vertex_pos_world_space;\n"
+
+	"out vec3 fragment_color;\n"
+
+	"uniform mat4 scene_model_view_projection;\n"
+
+	"void main() {\n"
+		"fragment_color = vertex_pos_world_space / 8.0f;\n"
+		"gl_Position = scene_model_view_projection * vec4(vertex_pos_world_space, 1.0f);\n"
+	"}\n",
+
+*const demo_22_scene_fragment_shader =
+	"#version 330 core\n"
+
+	"in vec3 fragment_color;\n"
+	"out vec3 color;\n"
+
+	"void main() {\n"
+		"color = fragment_color;\n"
+	"}\n";
+
+GLuint init_demo_23_scene_vbo(buffer_size_t* const num_scene_vertices) {
+	const GLfloat plane_size[2] = {10.0f, 8.0f}; // X and Z
+	const GLint num_components_per_vertex = 3;
+
+	#define OBJ_OFFSET(x, y, z) (x) + plane_size[0] * 0.5f, (y) + 2.0f, (z) + plane_size[1] * 0.5f
+
+	const GLfloat vertices[] = {
+		// Bottom left of flat plane
+		0.0f, 0.0f, 0.0f,
+		plane_size[0], 0.0f, 0.0f,
+		plane_size[0], 0.0f, plane_size[1],
+
+		// Top right of flat plane
+		0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, plane_size[1],
+		plane_size[0], 0.0f, plane_size[1],
+
+		OBJ_OFFSET(-1, -1, 0),
+		OBJ_OFFSET(0, 0, 0),
+		OBJ_OFFSET(1, -1, 0),
+	};
+
+	#undef OBJ_OFFSET
+
+	*num_scene_vertices = sizeof(vertices) / sizeof(vertices[0]) / num_components_per_vertex;
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, num_components_per_vertex, GL_FLOAT, GL_FALSE, 0, (void*) 0);
+
+	return vbo;
+}
 
 StateGL demo_23_init(void) {
 	StateGL sgl = {.vertex_array = init_vao(), .num_vertex_buffers = 0, .num_textures = 0};
 
-	SceneState scene_state = {.shadow_size = {1024, 1024}};
-	glGenFramebuffers(1, &scene_state.depth_map_framebuffer);
-	scene_state.depth_map_texture = preinit_texture(TexPlain, TexNonRepeating);
+	SceneState scene_state = {
+		.scene_shader = init_shader_program(demo_22_scene_vertex_shader, demo_22_scene_fragment_shader),
+		.depth_map_texture = preinit_texture(TexPlain, TexNonRepeating),
+		.shadow_size = {1024, 1024}
+	};
+
+	scene_state.scene_vbo = init_demo_23_scene_vbo(&scene_state.num_scene_vertices);
+
+	glUseProgram(scene_state.scene_shader);
+
+	//////////
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
 		scene_state.shadow_size[0], scene_state.shadow_size[1],
 		0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, NULL);
 
+	//////////
+
+	glGenFramebuffers(1, &scene_state.depth_map_framebuffer);
 	glBindFramebuffer(GL_FRAMEBUFFER, scene_state.depth_map_framebuffer);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, TexPlain, scene_state.depth_map_texture, 0);
 	glDrawBuffer(GL_NONE);
 	glReadBuffer(GL_NONE);
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-	//////////
-
-	glGenBuffers(1, &scene_state.scene_vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, scene_state.scene_vbo);
 
 	//////////
 
@@ -47,23 +114,27 @@ StateGL demo_23_init(void) {
 }
 
 void demo_23_drawer(const StateGL* const sgl) {
+	const SceneState scene_state = *((SceneState*) sgl -> any_data);
+
 	static Camera camera;
+	static GLint scene_model_view_projection_id;
 	static byte first_call = 1;
 
 	if (first_call) {
-		init_camera(&camera, (vec3) {0.0f, 0.0f, 0.0f});
+		INIT_UNIFORM(scene_model_view_projection, scene_state.scene_shader);
+		init_camera(&camera, (vec3) {0.0f, 1.0f, 0.0f});
 		first_call = 0;
 	}
+
 	update_camera(&camera, get_next_event(), NULL);
+	UPDATE_UNIFORM(scene_model_view_projection, Matrix4fv, 1, GL_FALSE, &camera.model_view_projection[0][0]);
 
-	const SceneState scene_state = *((SceneState*) sgl -> any_data);
-
-	// Rendering to depth map
+	// Rendering to depth map from light position
 	glViewport(0, 0, scene_state.shadow_size[0], scene_state.shadow_size[1]);
 	glBindFramebuffer(GL_FRAMEBUFFER, scene_state.depth_map_framebuffer);
 	glClear(GL_DEPTH_BUFFER_BIT);
 
-	// Render scene from light position
+	//////////
 
 	// Rendering as scene with shadow mapping, using depth map
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
@@ -72,16 +143,19 @@ void demo_23_drawer(const StateGL* const sgl) {
 
 	glBindTexture(TexPlain, scene_state.depth_map_texture);
 
-	// Render scene here
+	// Rendering scene here
+
+	glDrawArrays(GL_TRIANGLES, 0, scene_state.num_scene_vertices);
 }
 
 void demo_23_deinit(const StateGL* const sgl) {
-	SceneState* const scene_state = sgl -> any_data;
+	const SceneState scene_state = *((SceneState*) sgl -> any_data);
 
-	glDeleteBuffers(1, &scene_state -> scene_vbo);
-	glDeleteFramebuffers(1, &scene_state -> depth_map_framebuffer);
-	glDeleteTextures(1, &scene_state -> depth_map_texture);
-	free(scene_state);
+	glDeleteProgram(scene_state.scene_shader);
+	glDeleteBuffers(1, &scene_state.scene_vbo);
+	glDeleteFramebuffers(1, &scene_state.depth_map_framebuffer);
+	glDeleteTextures(1, &scene_state.depth_map_texture);
+	free(sgl -> any_data);
 
 	deinit_demo_vars(sgl);
 }
