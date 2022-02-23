46ac26b6237aca5c17770ccccd096988c740d4f2
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-02-23 02:49:49 -0500

    Change a texture filtering mode

--- src/audio_visual/accel/src/shadow_map.c
+++ src/audio_visual/accel/src/shadow_map.c
@@ -37,8 +37,8 @@ ShadowMapContext init_shadow_map_context(
 
 	glGenTextures(1, &texture);
 	glBindTexture(TexPlain, texture);
-	glTexParameteri(TexPlain, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(TexPlain, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(TexPlain, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(TexPlain, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
 	glTexParameteri(TexPlain, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
 	glTexParameteri(TexPlain, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
