27eaecfb1ec21c33d2c832e2078f044559d3d103
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-04-10 18:57:11 -0400

    Refactor the normal map generation code a lot. Optimize the surface usage so that only two intermediate surfaces are needed for the whole process, saving 1.25mb of memory. Because of that, there is no need for a horizontal blur buffer for the gaussian blur context, or saving the size of the surfaces being blurred. So, there is no more gaussian blur context, o, or `blur_surface` (it simply takes up space). Redo some spacing, especially in regards to whitespace. Change some var names to make them clearer.

--- src/audio_visual/accel/src/headers/normal_map_generation.h
+++ src/audio_visual/accel/src/headers/normal_map_generation.h
@@ -1,23 +1,8 @@
 #ifndef NORMAL_MAP_GENERATION_H
 #define NORMAL_MAP_GENERATION_H
 
-#include <SDL2/SDL.h>
-
 /* Excluded: limit_int_to_domain, read_surface_pixel, edge_checked_read_surface_pixel,
-sobel_sample, generate_normal_map, compute_1D_gaussian_kernel, do_separable_gaussian_blur_pass,
-init_gaussian_blur_context, deinit_gaussian_blur_context, blur_surface */
-
-/* The Gaussian blur performed is separable, so it needs a temporary surface for the horizontal pass.
-TODO: make this struct private in some way from users of the .c file */
-typedef struct {
-    const struct {
-        SDL_Surface* const horizontal;
-        const int size[2];
-    } blur_buffer;
-
-    float* const kernel;
-    const int kernel_radius;
-} GaussianBlurContext;
+sobel_sample, generate_normal_map, compute_1D_gaussian_kernel, do_separable_gaussian_blur_pass */
 
 GLuint init_normal_map_set_from_texture_set(const GLuint texture_set);
 
--- src/audio_visual/accel/src/normal_map_generation.c
+++ src/audio_visual/accel/src/normal_map_generation.c
@@ -20,11 +20,10 @@ static void* read_surface_pixel(const SDL_Surface* const surface, const int x, c
 static void* edge_checked_read_surface_pixel(const SDL_Surface* const surface, int x, int y) {
 	x = limit_int_to_domain(x, 0, surface -> w - 1);
 	y = limit_int_to_domain(y, 0, surface -> h - 1);
-
 	return read_surface_pixel(surface, x, y);
 }
 
-static float sobel_sample(SDL_Surface* const surface, const int x, const int y) {
+static float sobel_sample(const SDL_Surface* const surface, const int x, const int y) {
 	const sdl_pixel_t pixel = *(sdl_pixel_t*) edge_checked_read_surface_pixel(surface, x, y);
 
 	sdl_pixel_component_t r, g, b;
@@ -40,29 +39,27 @@ static float sobel_sample(SDL_Surface* const surface, const int x, const int y) 
 - https://www.shadertoy.com/view/Xtd3DS
 
 Also, this function computes luminance values
-of pixels to use those as heightmap values. */
-static void generate_normal_map(SDL_Surface* const src, SDL_Surface* const normal_map, const float intensity) {
-	const int src_w = src -> w, src_h = src -> h;
+of pixels to use those as heightmap values.
 
-	const SDL_PixelFormat *const dest_format = normal_map -> format;
+It's assumed that `src` has the same size as `dest`. */
+static void generate_normal_map(SDL_Surface* const src, SDL_Surface* const dest, const float intensity) {
+	const int w = src -> w, h = src -> h;
+
+	const SDL_PixelFormat* const dest_format = dest -> format;
 	const float one_over_intensity = 1.0f / intensity;
 
 	WITH_SURFACE_PIXEL_ACCESS(src,
-		WITH_SURFACE_PIXEL_ACCESS(normal_map,
+		WITH_SURFACE_PIXEL_ACCESS(dest,
 
-			for (int y = 0; y < src_h; y++) {
-				for (int x = 0; x < src_w; x++) {
+			for (int y = 0; y < h; y++) {
+				sdl_pixel_t* dest_pixel = read_surface_pixel(dest, 0, y);
+
+				for (int x = 0; x < w; x++, dest_pixel++) {
 					const float
-						tl = sobel_sample(src, x - 1, y - 1),
-						tm = sobel_sample(src, x,     y - 1),
-						tr = sobel_sample(src, x + 1, y - 1),
-
-						ml = sobel_sample(src, x - 1,     y),
-						mr = sobel_sample(src, x + 1,     y),
-
-						bl = sobel_sample(src, x - 1, y + 1),
-						bm = sobel_sample(src, x,     y + 1),
-						br = sobel_sample(src, x + 1, y + 1);
+						tl = sobel_sample(src, x - 1, y - 1), tm = sobel_sample(src, x, y - 1),
+						tr = sobel_sample(src, x + 1, y - 1), ml = sobel_sample(src, x - 1, y),
+						mr = sobel_sample(src, x + 1, y), bl = sobel_sample(src, x - 1, y + 1),
+						bm = sobel_sample(src, x, y + 1), br = sobel_sample(src, x + 1, y + 1);
 
 					vec3 normal = {
 						(-tl - ml * 2.0f - bl) + (tr + mr * 2.0f + br),
@@ -75,14 +72,11 @@ static void generate_normal_map(SDL_Surface* const src, SDL_Surface* const norma
 					// Converting normal from range of (-1, 1) to (0, 1), and then to (0, `max_byte_value`)
 					for (byte i = 0; i < 3; i++) normal[i] = (normal[i] * 0.5f + 0.5f) * constants.max_byte_value;
 
-					const sdl_pixel_t normal_vector_in_rgb_format = SDL_MapRGB(
-						dest_format,
+					*dest_pixel = SDL_MapRGB(dest_format,
 						(sdl_pixel_component_t) normal[0],
 						(sdl_pixel_component_t) normal[1],
 						(sdl_pixel_component_t) normal[2]
 					);
-
-					*(sdl_pixel_t*) read_surface_pixel(normal_map, x, y) = normal_vector_in_rgb_format;
 				}
 			}
 		);
@@ -91,15 +85,15 @@ static void generate_normal_map(SDL_Surface* const src, SDL_Surface* const norma
 
 ////////// This code concerns Gaussian blur (the normal map input is blurred to cut out high frequencies from the Sobel operator).
 
-static float* compute_1D_gaussian_kernel(const int radius, const float sigma) {
+static float* compute_1D_gaussian_kernel(const int radius, const float std_dev) {
 	const int kernel_length = radius * 2 + 1;
 
 	float* const kernel = malloc((size_t) kernel_length * sizeof(float)), sum = 0.0f;
-	const float one_over_two_times_sigma_squared = 1.0f / (2.0f * sigma * sigma);
+	const float one_over_two_times_std_dev_squared = 1.0f / (2.0f * std_dev * std_dev);
 
 	for (int x = 0; x < kernel_length; x++) {
 		const int dx = x - radius;
-		const float weight = expf(-(dx * dx) * one_over_two_times_sigma_squared);
+		const float weight = expf(-(dx * dx) * one_over_two_times_std_dev_squared);
 		kernel[x] = weight;
 		sum += weight;
 	}
@@ -110,7 +104,7 @@ static float* compute_1D_gaussian_kernel(const int radius, const float sigma) {
 	return kernel;
 }
 
-// This function assumes that `src` and `dest` have the same size (which should always be the case).
+// It's assumed that `src` has the same size as `dest`.
 static void do_separable_gaussian_blur_pass(SDL_Surface* const src,
 	SDL_Surface* const dest, const float* const kernel,
 	const int kernel_radius, const bool blur_is_vertical) {
@@ -127,20 +121,20 @@ static void do_separable_gaussian_blur_pass(SDL_Surface* const src,
 		WITH_SURFACE_PIXEL_ACCESS(dest,
 
 			for (int y = 0; y < h; y++) {
-				for (int x = 0; x < w; x++) {
+				sdl_pixel_t* dest_pixel = read_surface_pixel(dest, 0, y);
 
+				for (int x = 0; x < w; x++, dest_pixel++) {
 					float normalized_summed_channels[3] = {0.0f, 0.0f, 0.0f};
 
 					for (int i = -kernel_radius; i <= kernel_radius; i++) {
-
 						int filter_pos[2] = {x, y};
 						filter_pos[blur_is_vertical] += i; // If blur is vertical, `blur_is_vertical` equals 1; otherwise, 0
 
-						const sdl_pixel_t pixel = *(sdl_pixel_t*)
+						const sdl_pixel_t src_pixel = *(sdl_pixel_t*)
 							edge_checked_read_surface_pixel(src, filter_pos[0], filter_pos[1]);
 
 						sdl_pixel_component_t r, g, b;
-						SDL_GetRGB(pixel, src_format, &r, &g, &b);
+						SDL_GetRGB(src_pixel, src_format, &r, &g, &b);
 
 						const float one_over_max_byte_value_times_weight = one_over_max_byte_value * kernel[i + kernel_radius];
 						normalized_summed_channels[0] += r * one_over_max_byte_value_times_weight;
@@ -148,7 +142,7 @@ static void do_separable_gaussian_blur_pass(SDL_Surface* const src,
 						normalized_summed_channels[2] += b * one_over_max_byte_value_times_weight;
 					}
 
-					*(sdl_pixel_t*) read_surface_pixel(dest, x, y) = SDL_MapRGB(dest_format,
+					*dest_pixel = SDL_MapRGB(dest_format,
 						(sdl_pixel_component_t) (normalized_summed_channels[0] * constants.max_byte_value),
 						(sdl_pixel_component_t) (normalized_summed_channels[1] * constants.max_byte_value),
 						(sdl_pixel_component_t) (normalized_summed_channels[2] * constants.max_byte_value)
@@ -159,46 +153,15 @@ static void do_separable_gaussian_blur_pass(SDL_Surface* const src,
 	);
 }
 
-static GaussianBlurContext init_gaussian_blur_context(const float sigma,
-	const int radius, const int blur_buffer_w, const int blur_buffer_h) {
-
-	return (GaussianBlurContext) {
-		.blur_buffer = {
-			.horizontal = init_blank_surface(blur_buffer_w, blur_buffer_h, SDL_PIXEL_FORMAT),
-			.size = {blur_buffer_w, blur_buffer_h}
-		},
-
-		.kernel = compute_1D_gaussian_kernel(radius, sigma),
-		.kernel_radius = radius
-	};
-}
-
-static void deinit_gaussian_blur_context(const GaussianBlurContext* const context) {
-	deinit_surface(context -> blur_buffer.horizontal);
-	free(context -> kernel);
-}
-
-static SDL_Surface* blur_surface(SDL_Surface* const src, const GaussianBlurContext context) {
-	SDL_Surface
-		*const horizontal_blur_buffer = context.blur_buffer.horizontal,
-		*const vertical_blur_buffer = init_blank_surface(src -> w, src -> h, SDL_PIXEL_FORMAT);
-
-	do_separable_gaussian_blur_pass(src, horizontal_blur_buffer, context.kernel, context.kernel_radius, 0);
-	do_separable_gaussian_blur_pass(horizontal_blur_buffer, vertical_blur_buffer, context.kernel, context.kernel_radius, 1);
-
-	return vertical_blur_buffer;
-}
-
 GLuint init_normal_map_set_from_texture_set(const GLuint texture_set) {
 	/* How this function works:
+
 	- First, query OpenGL about information about the texture set, like its dimensions, and its filters used.
-	- Then, copy over its contents into a big surface.
-
-	- Blur it via a two-pass Gaussian blur, and then generate a normal map from that.
-		Note that the generated normal map is written into the original CPU-side surface, since that isn't used
-		anymore at this point, and creating a new surface would be wasteful.
-
-	- After that, reupload the normal map to the GPU via a new texture.
+	- Then, define two general purpose surfaces, #1 and #2.
+	- Copy the texture contents into surface #1.
+	- Blur #1 horizontally to #2, and blur #2 vertically to #1.
+	- Generate a normal map of #1 to #2.
+	- After that, reupload #2 to the GPU as a texture set of normal maps.
 
 	Note: normal maps are not interleaved with the texture set because if gamma correction is used,
 	the texture set will be in SRGB, and normal maps should be in a linear color space. */
@@ -219,47 +182,49 @@ GLuint init_normal_map_set_from_texture_set(const GLuint texture_set) {
 
 	////////// Uploading the texture to the CPU
 
-	const GLint surfaces_w = subtexture_w, surfaces_h = subtexture_h * num_subtextures;
-	SDL_Surface* const cpu_side_surfaces = init_blank_surface(surfaces_w, surfaces_h, SDL_PIXEL_FORMAT);
-	sdl_pixel_t* const cpu_side_surface_pixels = cpu_side_surfaces -> pixels;
+	const GLint general_purpose_surfaces_h = subtexture_h * num_subtextures;
 
-	WITH_SURFACE_PIXEL_ACCESS(cpu_side_surfaces,
+	SDL_Surface
+		*const general_purpose_surface_1 = init_blank_surface(subtexture_w, general_purpose_surfaces_h, SDL_PIXEL_FORMAT),
+		*const general_purpose_surface_2 = init_blank_surface(subtexture_w, general_purpose_surfaces_h, SDL_PIXEL_FORMAT);
+
+	// Copying the original texture set to #1
+	WITH_SURFACE_PIXEL_ACCESS(general_purpose_surface_1,
 		glGetTexImage(TexSet, 0, OPENGL_INPUT_PIXEL_FORMAT,
-			OPENGL_COLOR_CHANNEL_TYPE, cpu_side_surface_pixels);
+			OPENGL_COLOR_CHANNEL_TYPE, general_purpose_surface_1 -> pixels);
 	);
 
-	////////// Blurring it, and then making a normal map out of it
+	////////// Blurring it, and then making a normal map
 
-	const GaussianBlurContext blur_context = init_gaussian_blur_context(
-		constants.normal_mapping.blur.std_dev, constants.normal_mapping.blur.radius, surfaces_w, surfaces_h);
+	const int blur_radius = constants.normal_mapping.blur.radius;
+	float* const blur_kernel = compute_1D_gaussian_kernel(blur_radius, constants.normal_mapping.blur.std_dev);
+	// Blurring #1 to #2 horizontally, and then blurring #2 vertically to #1
+	do_separable_gaussian_blur_pass(general_purpose_surface_1, general_purpose_surface_2, blur_kernel, blur_radius, false);
+	do_separable_gaussian_blur_pass(general_purpose_surface_2, general_purpose_surface_1, blur_kernel, blur_radius, true);
+	free(blur_kernel);
 
-	SDL_Surface* const blurred = blur_surface(cpu_side_surfaces, blur_context);
+	generate_normal_map(general_purpose_surface_1, general_purpose_surface_2, constants.normal_mapping.intensity);
 
-	/* This reuses `cpu_side_surfaces` as a normal map, to save memory.
-	`cpu_side_surfaces` is considered out of scope after this. */
-	SDL_Surface* const normal_map = cpu_side_surfaces;
-	generate_normal_map(blurred, normal_map, constants.normal_mapping.intensity);
-
-	////////// Making a new texture on the GPU, and then writing the normal map into that
+	////////// Making a new texture on the GPU, and then writing the normal map to that
 
 	const GLuint normal_map_set = preinit_texture(TexSet,
 		(TextureWrapMode) wrap_mode,
 		(TextureFilterMode) mag_filter,
 		(TextureFilterMode) min_filter);
 
-	WITH_SURFACE_PIXEL_ACCESS(normal_map,
+	// Copying #2 to a new texture on the GPU
+	WITH_SURFACE_PIXEL_ACCESS(general_purpose_surface_2,
 		glTexImage3D(TexSet, 0, OPENGL_NORMAL_MAP_INTERNAL_PIXEL_FORMAT, subtexture_w,
 			subtexture_h, num_subtextures, 0, OPENGL_INPUT_PIXEL_FORMAT,
-			OPENGL_COLOR_CHANNEL_TYPE, normal_map -> pixels);
+			OPENGL_COLOR_CHANNEL_TYPE, general_purpose_surface_2 -> pixels);
 	);
 
 	glGenerateMipmap(TexSet);
 
 	////////// Deinitialization
 
-	deinit_surface(normal_map);
-	deinit_surface(blurred);
-	deinit_gaussian_blur_context(&blur_context);
+	deinit_surface(general_purpose_surface_1);
+	deinit_surface(general_purpose_surface_2);
 
 	return normal_map_set;
 }
