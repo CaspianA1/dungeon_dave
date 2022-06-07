bdd03429d95e78953d34d7ec9235bdb0e7f12c2f
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-06-07 18:51:55 -0400

    Leave an important todo

--- src/level.c
+++ src/level.c
@@ -8,6 +8,8 @@
 - Read into a LevelDescription struct. Input validation done there.
 - General context created from that.
 - Light params in the LevelDescription struct go to the GPU.
+
+- Perhaps go directly from reading JSON to creating the level context?
 */
 
 #endif
