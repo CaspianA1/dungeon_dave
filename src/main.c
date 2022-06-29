38de100f505c0f8a79ab2dda2860ed6ffb27af9c
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-06-29 17:47:23 -0400

    Add back a missed call to update_billboards. Move the draw_skybox call into the depth-buffer-write-disabled code chunk.

--- src/main.c
+++ src/main.c
@@ -194,19 +194,20 @@ static void main_drawer(void* const app_context) {
 	Camera* const camera = &scene_context -> camera;
 
 	update_camera(camera, event, scene_context -> heightmap, scene_context -> map_size);
+	update_billboards(billboard_context);
+	update_weapon_sprite(weapon_sprite, camera, &event);
 
 	//////////
 
-	update_weapon_sprite(weapon_sprite, camera, &event);
 	draw_to_shadow_context(shadow_context, camera, event.screen_size, draw_all_sectors_for_shadow_map, &sector_context -> draw_context);
 
 	////////// The main drawing code
 
-	draw_skybox(&scene_context -> skybox, camera -> view_projection); // Skybox after sectors + billboards to avoid a lot of unnecessary drawing
 	draw_visible_sectors(sector_context, shadow_context, camera, event.screen_size);
+	draw_visible_billboards(billboard_context, shadow_context, camera);
 
 	WITH_RENDER_STATE(glDepthMask, GL_FALSE, GL_TRUE, // Not writing to the depth buffer for these
-		draw_visible_billboards(billboard_context, shadow_context, camera);
+		draw_skybox(&scene_context -> skybox, camera -> view_projection);
 		draw_weapon_sprite(weapon_sprite, camera, shadow_context);
 	);
 }
