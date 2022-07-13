651d65f4ac913dda838bab29118c992caa471db8
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-07-13 09:25:14 -0400

    Revise the drawing architecture change plan

--- src/headers/main.h
+++ src/headers/main.h
@@ -11,24 +11,26 @@
 #include "title_screen.h"
 
 /* Drawing architecture change, plan:
-1. Figure out how to get a texture with an alpha channel to render to the shadow map
+1. Allow BatchDrawContext to call glDrawArraysInstanced, if needed
+2. Abandon BatchDrawContext, and replace the current frustum culling code with some code that uses details from Drawable
 
-3. Figure out how to render the weapon sprite to the shadow map
-4. If needed, render a flat character or 3D character model with it, so that the weapon isn't floating
+3. Figure out how to get a texture with an alpha channel to render to the shadow map
 
-5. Add a dependency injection step for Drawable that allows a shadow map rendering step.
+4. Figure out how to render the weapon sprite to the shadow map
+5. If needed, render a flat character or 3D character model with it, so that the weapon isn't floating
+
+6. Add a dependency injection step for Drawable that allows a shadow map rendering step.
 	Use that step for sectors now, and billboards and the weapon sprite later
 
-6. Make a definitive plan on how to render billboards to the shadow map, and then do so
-7. Make a sector mesh buffer optimized for the shadow map: back faces pre-culled,
+7. Make a definitive plan on how to render billboards to the shadow map, and then do so
+8. Make a sector mesh buffer optimized for the shadow map: back faces pre-culled,
 	face info bits removed, map edge faces added, and in a GL_STATIC_DRAW buffer
 
-8.  Make an R-tree implementation for static objects, namely sectors here
-9.  Make it so that the R-tree can be dynamic too, so for billboards
-10. Replace `cull_from_frustum_into_gpu_buffer` with this recursive culling -> GPU buffer method
-11. Remove BatchDrawContext, and replace it with this R-tree code as a new type of context
-12. Allow this new type of context to act as a dependency injection step into Drawable,
-	so that in the end, everything uses Drawable
+9.  Make an R-tree implementation for static objects, namely sectors here
+10.  Make it so that the R-tree can be dynamic too, so for billboards
+11. Replace `cull_from_frustum_into_gpu_buffer` with this recursive culling -> GPU buffer method
+12. Allow frustum culling to act as a dependency injection step into Drawable, so that in the end,
+	everything uses Drawable
 */
 
 // TODO: add more const qualifiers where I can
