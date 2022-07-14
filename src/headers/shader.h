857cce797fa139c1492e5171a6713b0efc42c0f5
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-07-14 07:38:27 -0400

    Add a file I forgot to commit in the last commit. Change the name of fail_on_shader_creation_error to fail_on_sub_shader_creation_error.

--- src/headers/shader.h
+++ src/headers/shader.h
@@ -6,7 +6,7 @@
 #define use_shader glUseProgram
 #define deinit_shader glDeleteProgram
 
-/* Excluded: fail_on_shader_creation_error, init_shader_from_source,
+/* Excluded: fail_on_sub_shader_creation_error, init_shader_from_source,
 read_file_contents, get_source_for_included_file, read_and_parse_includes_for_glsl,
 erase_version_strings_from_dependency_list */
 
