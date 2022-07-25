ddd2714bdf98f563461655d9cbea0bf38b9a3deb
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-07-25 06:32:51 -0400

    Update the macos install script according to another branch

--- install_scripts/macos.sh
+++ install_scripts/macos.sh
@@ -1,14 +1,26 @@
 # This script assumes that the developer tools are installed.
 # That should include clang, make, and git.
 
-brew install pkg-config sdl2 cmake
+build_from_github() {
+	repo_name=$1
+	project_name=$2
 
-git clone git@github.com:recp/cglm.git
-cd cglm
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
-cd ../..
-sudo rm -r cglm
+	git clone git@github.com:$repo_name/$project_name.git
+
+	cd $project_name
+	mkdir build
+	cd build
+	cmake ..
+	make
+	sudo make install
+	cd ../..
+	sudo rm -r $project_name
+}
+
+main() {
+	brew install pkg-config sdl2 cmake
+	build_from_github recp cglm
+	build_from_github DaveGamble cJSON
+}
+
+main
