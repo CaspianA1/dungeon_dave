3123378ddb4b1e848a6d7ff2cc51400a988b4f0b
Caspian Ahlberg <caspian@cloudflare.com>
2022-08-09 12:31:05 -0400

    Make a blank test map for AO

--- include/data/maps.h
+++ include/data/maps.h
@@ -17,7 +17,8 @@ enum {
 	test_width = 8, test_height = 5,
 	terrain_width = 255, terrain_height = 255,
 	tiny_width = 7, tiny_height = 7,
-	checker_width = 255, checker_height = 255
+	checker_width = 255, checker_height = 255,
+	blank_width = 1, blank_height = 2
 };
 
 #define MAP_DEF(prefix) prefix##_heightmap[prefix##_height][prefix##_width], prefix##_texture_id_map[prefix##_height][prefix##_width]
@@ -25,7 +26,7 @@ enum {
 extern const byte
 	MAP_DEF(level_one), MAP_DEF(architecture), MAP_DEF(fortress),
 	MAP_DEF(palace), MAP_DEF(tpt), MAP_DEF(pyramid), MAP_DEF(maze),
-	MAP_DEF(test), MAP_DEF(terrain), MAP_DEF(tiny), MAP_DEF(checker);
+	MAP_DEF(test), MAP_DEF(terrain), MAP_DEF(tiny), MAP_DEF(checker), MAP_DEF(blank);
 
 #undef MAP_DEF
 
