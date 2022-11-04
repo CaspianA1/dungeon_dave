55ec12ea54b8a6bcd81347eb8a4af04fc09bb6cb
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-11-04 13:38:44 -0400

    Change a pragma a bit

--- include/utils/buffer_defs.h
+++ include/utils/buffer_defs.h
@@ -11,14 +11,14 @@
 
 #include "lib/cglm/cglm.h"
 
+#pragma clang diagnostic pop
+
 // If SDL2/SDL.h isn't found, try SDL.h
 #if __has_include("SDL2/SDL.h")
 	#include <SDL2/SDL.h>
 #else
 	#include <SDL.h>
 #endif
-
-#pragma clang diagnostic pop
 
 //////////
 
