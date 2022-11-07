36ec2708b17b65bc75ca68d4f3edac938ae45f18
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-11-06 19:19:00 -0500

    Now inferring the bytes per pixel for 'read_surface_pixel'

--- src/utils/texture.c
+++ src/utils/texture.c
@@ -27,7 +27,7 @@ SDL_Surface* init_surface(const GLchar* const path) {
 
 void* read_surface_pixel(const SDL_Surface* const surface, const GLint x, const GLint y) {
 	sdl_pixel_component_t* const row = (sdl_pixel_component_t*) surface -> pixels + y * surface -> pitch;
-	return row + x * (GLint) sizeof(sdl_pixel_t);
+	return row + x * surface -> format -> BytesPerPixel;
 }
 
 static void premultiply_surface_alpha(SDL_Surface* const surface) {
