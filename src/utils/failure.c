a2937266a3a3ab2ed19cef35ece5d71991ff5a62
Caspian Ahlberg <caspianahlberg@gmail.com>
2022-12-12 15:45:34 -0500

    Make a repeated value a variable

--- src/utils/failure.c
+++ src/utils/failure.c
@@ -4,13 +4,15 @@
 void print_failure_message(const char* const failure_type_string,
 	const char* const format_string, const char* const filename, const int line_number, ...) {
 
-	fprintf(stderr, "Failed with error type '%s' in source file '%s' on line %d. Reason: '",
+	FILE* const out = stderr;
+
+	fprintf(out, "Failed with error type '%s' in source file '%s' on line %d. Reason: '",
 		failure_type_string, filename, line_number);
 	
 	va_list format_args;
 	va_start(format_args, line_number);
-		vfprintf(stderr, format_string, format_args);
+		vfprintf(out, format_string, format_args);
 	va_end(format_args);
 
-	fputs("'.\n", stderr);
+	fputs("'.\n", out);
 }
