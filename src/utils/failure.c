d35b8c46752d859113763f3d2b3326de897dbb12
Caspian Ahlberg <caspianahlberg@gmail.com>
2023-01-24 15:14:39 -0500

    Add a comment, leave a TODO, and change some formatting

--- src/utils/failure.c
+++ src/utils/failure.c
@@ -1,6 +1,7 @@
 #include "utils/failure.h"
-#include <stdarg.h>
+#include <stdarg.h> // For variadic utils
 
+// TODO: print out a dialogue box (get the window via `SDL_GL_GetCurrentWindow`, and then use `SDL_ShowSimpleMessageBox`)
 void print_failure_message(const char* const failure_type_string,
 	const char* const format_string, const char* const filename, const int line_number, ...) {
 
@@ -10,8 +11,9 @@ void print_failure_message(const char* const failure_type_string,
 		failure_type_string, filename, line_number);
 	
 	va_list format_args;
+
 	va_start(format_args, line_number);
-		vfprintf(out, format_string, format_args);
+	vfprintf(out, format_string, format_args);
 	va_end(format_args);
 
 	fputs("'.\n", out);
