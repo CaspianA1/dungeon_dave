cbe6aad81d43e258379418bf1fde757d26fddc47
Caspian Ahlberg <caspianahlberg@gmail.com>
2023-11-30 17:39:04 -0500

    Make the asset path a global constant. Leave 2 TODOs. Rearrange some arithmetic.

--- include/utils/safe_io.h
+++ include/utils/safe_io.h
@@ -5,6 +5,9 @@
 #include "utils/failure.h" // For `FAIL`
 #include "utils/alloc.h" // For `alloc`
 
+// TODO: put in `constants.h`?
+static const char* const ASSET_PATH_PREFIX = "../../assets/";
+
 /* Why this is used:
 - All assets in the `assets` directory share the common prefix defined in this function.
 Therefore, it's wasteful to repeat this string across every asset used, and if the path changes
@@ -14,19 +17,20 @@ for whatever reason, then one would have to manually change all of the path pref
 that must be fully used before the function is called again. Therefore, it is encouraged
 to call this function as late as possible, in terms of when the asset path is needed. */
 static inline const char* get_temp_asset_path(const char* const unmodified_path) {
-	// TODO: try to make these constants in `constants.h`
-	static const char* const path_prefix = "../../assets/";
+	// TODO: put in `constants.h`?
 	enum {max_concatenation_buffer_size = 100u};
 
 	static char temp_concatenated_string[max_concatenation_buffer_size];
 
-	const size_t num_output_bytes = strlen(unmodified_path) + strlen(path_prefix) + 1u; // 1 more for the null terminator
+	// Adding 1 at the end for the null terminator
+	const size_t num_output_bytes = strlen(ASSET_PATH_PREFIX) + strlen(unmodified_path) + 1u;
 
 	if (num_output_bytes > max_concatenation_buffer_size)
 		FAIL(OpenFile, "Cannot open the file with path '%s', since its path when prefixed"
-			" with '%s' exceeds the max path length", unmodified_path, path_prefix);
+			" with '%s' exceeds the max path length", unmodified_path, ASSET_PATH_PREFIX);
 
-	strcpy(temp_concatenated_string, path_prefix);
+	// TODO: use memcpy here instead
+	strcpy(temp_concatenated_string, ASSET_PATH_PREFIX);
 	strcat(temp_concatenated_string, unmodified_path);
 
 	return temp_concatenated_string;
